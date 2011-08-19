a0e8a43126ff1fb786f98606f3bedba9c476b982
Julian Brost <julian.brost@googlemail.com>
2011-08-19 20:26:05 +0200

    Added some boilerplate activities.

---
+++ src/net/Ox4a42/volksempfaenger/SettingsActivity.java
@@ -0,0 +1,12 @@
+package net.Ox4a42.volksempfaenger;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingsActivity extends Activity {
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+	}
+}
--- src/net/Ox4a42/volksempfaenger/VolksempfaengerActivity.java
+++ src/net/Ox4a42/volksempfaenger/VolksempfaengerActivity.java
@@ -1,6 +1,7 @@
 package net.Ox4a42.volksempfaenger;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -30,18 +31,28 @@ public class VolksempfaengerActivity extends Activity implements OnClickListener
     }
 
 	public void onClick(View v) {
+		Intent intent;
+		
 		switch (v.getId()) {
 		case R.id.button_subscriptionlist:
 			Toast.makeText(this, "SubscriptionListActivity", Toast.LENGTH_SHORT).show();
+			intent = new Intent(this, SubscriptionListActivity.class);
+			startActivity(intent);
 			break;
 		case R.id.button_listenqueue:
 			Toast.makeText(this, "ListenQueueActivity", Toast.LENGTH_SHORT).show();
+			intent = new Intent(this, ListenQueueActivity.class);
+			startActivity(intent);
 			break;
 		case R.id.button_downloadqueue:
 			Toast.makeText(this, "DownloadQueueActivity", Toast.LENGTH_SHORT).show();
+			intent = new Intent(this, DownloadQueueActivity.class);
+			startActivity(intent);
 			break;
 		case R.id.button_settings:
 			Toast.makeText(this, "SettingsActivity", Toast.LENGTH_SHORT).show();
+			intent = new Intent(this, SettingsActivity.class);
+			startActivity(intent);
 			break;
 		}
 	}
