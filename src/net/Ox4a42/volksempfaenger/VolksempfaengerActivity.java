dc3e3660e3305709a5110437b19c2da593e90881
Julian Brost <julian.brost@googlemail.com>
2011-08-19 19:47:23 +0200

    Created new Android project.

---
+++ src/net/Ox4a42/volksempfaenger/VolksempfaengerActivity.java
@@ -0,0 +1,14 @@
+package net.Ox4a42.volksempfaenger;
+
+import net.Ox4a42.volksempfaenger.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VolksempfaengerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
