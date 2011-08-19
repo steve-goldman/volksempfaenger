743dd9d85d08bd1396b7e304c3435555e5d764d4
Julian Brost <julian.brost@googlemail.com>
2011-08-20 01:07:32 +0200

    Added DbHelper class.

--- src/net/Ox4a42/volksempfaenger/VolksempfaengerApplication.java
+++ src/net/Ox4a42/volksempfaenger/VolksempfaengerApplication.java
@@ -1,10 +1,29 @@
 package net.Ox4a42.volksempfaenger;
 
+import net.Ox4a42.volksempfaenger.data.DbHelper;
 import android.app.Application;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 public class VolksempfaengerApplication extends Application {
+	private SharedPreferences settings;
+	private DbHelper dbhelper;
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		settings = getSharedPreferences(null, MODE_PRIVATE);
+		dbhelper = new DbHelper(this);
+		// Just to ensure that the database gets created.
+		// TODO: Remove this later
+		dbhelper.getReadableDatabase();
+	}
+
 	public SharedPreferences getSharedPreferences() {
-		return getSharedPreferences(null, MODE_PRIVATE);
+		return settings;
+	}
+	
+	public DbHelper getDbHelper() {
+		return dbhelper;
 	}
 }
