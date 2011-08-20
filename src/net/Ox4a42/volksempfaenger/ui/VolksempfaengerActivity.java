d093f24a401a8815fa6189517e5015088e54f5f3
Julian Brost <julian.brost@googlemail.com>
2011-08-20 19:27:04 +0200

    Moved FeedParser test code to its own method and added a button for it.

--- src/net/Ox4a42/volksempfaenger/ui/VolksempfaengerActivity.java
+++ src/net/Ox4a42/volksempfaenger/ui/VolksempfaengerActivity.java
@@ -21,52 +21,38 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class VolksempfaengerActivity extends BaseActivity implements OnClickListener {
-	private static String TAG = "VolksempfaengerActivity";
+public class VolksempfaengerActivity extends BaseActivity implements
+		OnClickListener {
 	private Button buttonAddSubscription;
-    private Button buttonSubscriptionList;
-    private Button buttonListenQueue;
-    private Button buttonDownloadQueue;
-    private Button buttonSettings;
-    
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        buttonAddSubscription = (Button) findViewById(R.id.button_addsubscription);
-        buttonSubscriptionList = (Button) findViewById(R.id.button_subscriptionlist);
-        buttonListenQueue = (Button) findViewById(R.id.button_listenqueue);
-        buttonDownloadQueue = (Button) findViewById(R.id.button_downloadqueue);
-        buttonSettings = (Button) findViewById(R.id.button_settings);
-        
-        buttonAddSubscription.setOnClickListener(this);
-        buttonSubscriptionList.setOnClickListener(this);
-        buttonListenQueue.setOnClickListener(this);
-        buttonDownloadQueue.setOnClickListener(this);
-        buttonSettings.setOnClickListener(this);
-        
-        try {
-			Feed feed = FeedParser.parse(new InputStreamReader(getResources().openRawResource(R.raw.atom_test)));
-			Log.d(TAG, "Title: " + feed.getTitle());
-			for(FeedItem item: feed.getItems()) {
-				Log.d(TAG, "Item title: " + item.getTitle());
-			}
-        } catch (NotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (XmlPullParserException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    }
+	private Button buttonSubscriptionList;
+	private Button buttonListenQueue;
+	private Button buttonDownloadQueue;
+	private Button buttonSettings;
+	private Button buttonTestFeed;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		buttonAddSubscription = (Button) findViewById(R.id.button_addsubscription);
+		buttonSubscriptionList = (Button) findViewById(R.id.button_subscriptionlist);
+		buttonListenQueue = (Button) findViewById(R.id.button_listenqueue);
+		buttonDownloadQueue = (Button) findViewById(R.id.button_downloadqueue);
+		buttonSettings = (Button) findViewById(R.id.button_settings);
+		buttonTestFeed = (Button) findViewById(R.id.button_testfeed);
+
+		buttonAddSubscription.setOnClickListener(this);
+		buttonSubscriptionList.setOnClickListener(this);
+		buttonListenQueue.setOnClickListener(this);
+		buttonDownloadQueue.setOnClickListener(this);
+		buttonSettings.setOnClickListener(this);
+		buttonTestFeed.setOnClickListener(this);
+	}
 
 	public void onClick(View v) {
 		Intent intent;
-		
+
 		switch (v.getId()) {
 		case R.id.button_addsubscription:
 			intent = new Intent(this, AddSubscriptionActivity.class);
@@ -88,21 +74,46 @@ public class VolksempfaengerActivity extends BaseActivity implements OnClickList
 			intent = new Intent(this, SettingsActivity.class);
 			startActivity(intent);
 			break;
+		case R.id.button_testfeed:
+			Toast.makeText(this, "Read the logcat", Toast.LENGTH_SHORT).show();
+			testFeedParser();
+			break;
 		}
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		addGlobalMenu(menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		switch(item.getItemId()) {
+		switch (item.getItemId()) {
 		default:
 			handleGlobalMenu(item);
 		}
 		return true;
+	}
+
+	public void testFeedParser() {
+		try {
+			Feed feed = FeedParser.parse(new InputStreamReader(getResources()
+					.openRawResource(R.raw.atom_test)));
+			Log.d(getClass().getCanonicalName(), "Title: " + feed.getTitle());
+			for (FeedItem item : feed.getItems()) {
+				Log.d(getClass().getCanonicalName(),
+						"Item title: " + item.getTitle());
+			}
+		} catch (NotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (XmlPullParserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
