ad72b7f61b24e677df95c82e3fe993eaee0c9f30
Vincent Sch??ler <vincent@vschuessler.org>
2011-08-20 17:46:14 +0200

    Add setters to Enclosure class

--- src/net/Ox4a42/volksempfaenger/feedparser/Enclosure.java
+++ src/net/Ox4a42/volksempfaenger/feedparser/Enclosure.java
@@ -1,23 +1,48 @@
 package net.Ox4a42.volksempfaenger.feedparser;
 
 public class Enclosure {
-	public FeedItem getFeedItem() {
-		return new FeedItem();
-	}
-	
+	private String title, url, mime;
+	private Integer length;
+	private FeedItem feedItem;
+
 	public String getTitle() {
-		return "EP001.mp3";
+		return title;
 	}
-	
+
 	public String getUrl() {
-		return "http://example.org/podcast/ep001.mp3";
+		return url;
 	}
-	
+
 	public String getMime() {
-		return "audio/mpeg";
+		return mime;
 	}
 	
 	public Integer getLength() {
-		return 10485760;
+		return length;
+	}
+	
+	public FeedItem getFeedItem() {
+		return feedItem;
+	}
+	
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	public void setMime(String mime) {
+		this.mime = mime;
+	}
+	
+	public void setLength(Integer length) {
+		this.length = length;
+	}
+	
+	public void setFeedItem(FeedItem feedItem) {
+		this.feedItem = feedItem;
 	}
 }
