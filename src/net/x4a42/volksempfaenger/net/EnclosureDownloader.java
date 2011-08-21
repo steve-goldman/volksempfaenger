9173feac07a879dd53ffe1e747e783e9dd0ddeb7
Julian Brost <julian.brost@googlemail.com>
2011-08-21 14:07:38 +0200

    Started implementing EnclosureDownloader.

--- src/net/x4a42/volksempfaenger/net/EnclosureDownloader.java
+++ src/net/x4a42/volksempfaenger/net/EnclosureDownloader.java
@@ -1,5 +1,35 @@
 package net.x4a42.volksempfaenger.net;
 
-public class EnclosureDownloader {
+import net.x4a42.volksempfaenger.R;
+import android.app.Activity;
+import android.app.DownloadManager;
+import android.app.DownloadManager.Query;
+import android.app.DownloadManager.Request;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+public class EnclosureDownloader {
+	private Context context;
+	private DownloadManager dm;
+
+	public EnclosureDownloader(Context context) {
+		this.context = context;
+		dm = (DownloadManager) context
+				.getSystemService(Context.DOWNLOAD_SERVICE);
+	}
+
+	public void downloadEnclosure(String url) {
+		Request request = new Request(Uri.parse(url));
+		dm.enqueue(request);
+
+		// registerReceiver(receiver, new IntentFilter(
+		// DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+	}
 }
