8160af349f369e729dbe52a87d89b938515a1aee
Julian Brost <julian.brost@googlemail.com>
2011-08-21 20:41:09 +0200

    Added BootCompletedReceiver and UpdateService.

---
+++ src/net/x4a42/volksempfaenger/data/UpdateService.java
@@ -0,0 +1,16 @@
+package net.x4a42.volksempfaenger.data;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class UpdateService extends Service {
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
