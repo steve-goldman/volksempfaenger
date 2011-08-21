8db36a1b70e91330252eed1829665eae8406a1f0
Julian Brost <julian.brost@googlemail.com>
2011-08-21 20:41:09 +0200

    Added BootCompletedReceiver and UpdateService.

---
+++ src/net/x4a42/volksempfaenger/data/BootCompletedReceiver.java
@@ -0,0 +1,17 @@
+package net.x4a42.volksempfaenger.data;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
