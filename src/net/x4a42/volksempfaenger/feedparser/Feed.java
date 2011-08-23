f539b7588122ec3fd982bdc9199ddca71bd67c39
Vincent Sch??ler <vincent@vschuessler.org>
2011-08-23 18:42:47 +0200

    FeedParser: Add support for feed icon

--- src/net/x4a42/volksempfaenger/feedparser/Feed.java
+++ src/net/x4a42/volksempfaenger/feedparser/Feed.java
@@ -3,44 +3,51 @@ package net.x4a42.volksempfaenger.feedparser;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Feed {
-	private String url, title, website, description;
+	private String url, title, website, description, image;
+
+	public String getImage() {
+		return image;
+	}
+
 	private List<FeedItem> items = new ArrayList<FeedItem>();
-	
+
 	public String getUrl() {
 		return url;
 	}
-	
+
 	public String getTitle() {
 		return title;
 	}
-	
+
 	public String getWebsite() {
 		return website;
 	}
-	
+
 	public String getDescription() {
 		return description;
 	}
-	
+
 	public List<FeedItem> getItems() {
 		return items;
 	}
 
+	public void setImage(String image) {
+		this.image = image;
+	}
 
 	public void setUrl(String url) {
 		this.url = url;
 	}
-	
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
+
 	public void setWebsite(String website) {
 		this.website = website;
 	}
-	
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
