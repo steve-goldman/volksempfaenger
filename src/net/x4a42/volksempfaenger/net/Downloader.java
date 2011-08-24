6c340313da1f8ece7c05522f167da1972dce2c4c
Julian Brost <julian.brost@googlemail.com>
2011-08-24 22:17:21 +0200

    Reimplemented downloaders using HttpURLConnection.

--- src/net/x4a42/volksempfaenger/net/Downloader.java
+++ src/net/x4a42/volksempfaenger/net/Downloader.java
@@ -1,17 +1,22 @@
 package net.x4a42.volksempfaenger.net;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import net.x4a42.volksempfaenger.R;
 import net.x4a42.volksempfaenger.VolksempfaengerApplication;
 import android.content.Context;
 
 public abstract class Downloader {
-	
+
 	private Context context;
-	
+
 	public Downloader(Context context) {
 		this.context = context;
 	}
-	
+
 	protected Context getContext() {
 		return context;
 	}
@@ -21,5 +26,15 @@ public abstract class Downloader {
 		String version = VolksempfaengerApplication.getPackageInfo(context).versionName;
 		return String.format("%s/%s", name, version);
 	}
-	
+
+	public HttpURLConnection getConnection(String url) throws IOException {
+		URL urlObj = new URL(url);
+		HttpURLConnection connection = (HttpURLConnection) urlObj
+				.openConnection();
+		connection.setRequestProperty("User-Agent", getUserAgent());
+		connection.setInstanceFollowRedirects(true);
+		connection.setUseCaches(true);
+		return connection;
+	}
+
 }
--- src/net/x4a42/volksempfaenger/net/FeedDownloader.java
+++ src/net/x4a42/volksempfaenger/net/FeedDownloader.java
@@ -1,18 +1,13 @@
 package net.x4a42.volksempfaenger.net;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 
 import net.x4a42.volksempfaenger.feedparser.Feed;
 import net.x4a42.volksempfaenger.feedparser.FeedParser;
 import net.x4a42.volksempfaenger.feedparser.FeedParserException;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-
 import android.content.Context;
-import android.net.http.AndroidHttpClient;
 import android.util.Log;
 
 public class FeedDownloader extends Downloader {
@@ -21,32 +16,21 @@ public class FeedDownloader extends Downloader {
 		super(context);
 	}
 
-	private AndroidHttpClient getHttpClient() {
-		return AndroidHttpClient.newInstance(getUserAgent(), getContext());
-	}
-
 	public Feed fetchFeed(String url) throws NetException, FeedParserException {
-		AndroidHttpClient client = getHttpClient();
-		HttpGet request;
 		try {
-			request = new HttpGet(url);
-		} catch (IllegalArgumentException e) {
-			Log.w(getClass().getSimpleName(), e);
-			throw new NetException(e);
-		}
-		request.addHeader("Accept", "application/atom+xml, application/rss+xml");
-		try {
-			HttpResponse response = client.execute(request);
+			HttpURLConnection connection = getConnection(url);
+
+			connection.setRequestProperty("Accept",
+					"application/atom+xml, application/rss+xml");
 			BufferedReader rd = new BufferedReader(new InputStreamReader(
-					response.getEntity().getContent()));
+					connection.getInputStream()));
 			Feed feed = FeedParser.parse(rd);
-			client.close();
+			connection.disconnect();
 			return feed;
-		} catch (IllegalStateException e) {
-			Log.w(getClass().getSimpleName(), e);
-			throw new NetException(e);
-		} catch (IOException e) {
-			Log.w(getClass().getSimpleName(), e);
+		} catch (FeedParserException e) {
+			throw e;
+		} catch (Exception e) {
+			Log.i(getClass().getSimpleName(), "Handled Exception:", e);
 			throw new NetException(e);
 		}
 	}
