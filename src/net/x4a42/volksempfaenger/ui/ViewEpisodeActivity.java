08f44940be6d4a152b9b7eee3c3d5847a597d6de
Julian Brost <julian.brost@googlemail.com>
2011-08-24 02:09:02 +0200

    ViewEpisodeActivity: Added ability to display images.

--- src/net/x4a42/volksempfaenger/ui/ViewEpisodeActivity.java
+++ src/net/x4a42/volksempfaenger/ui/ViewEpisodeActivity.java
@@ -1,13 +1,21 @@
 package net.x4a42.volksempfaenger.ui;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
 
 import net.x4a42.volksempfaenger.R;
 import net.x4a42.volksempfaenger.Utils;
 import net.x4a42.volksempfaenger.data.DatabaseHelper;
+import net.x4a42.volksempfaenger.net.DescriptionImageDownloader;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
@@ -25,6 +33,8 @@ public class ViewEpisodeActivity extends BaseActivity {
 	private TextView podcastDescription;
 	private TextView episodeTitle;
 	private TextView episodeDescription;
+
+	private String descriptionText;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -64,8 +74,8 @@ public class ViewEpisodeActivity extends BaseActivity {
 
 		// Update episode information
 
-		c = dbHelper.getReadableDatabase().query(DatabaseHelper.Episode._TABLE, null,
-				String.format("%s = ?", DatabaseHelper.Episode.ID),
+		c = dbHelper.getReadableDatabase().query(DatabaseHelper.Episode._TABLE,
+				null, String.format("%s = ?", DatabaseHelper.Episode.ID),
 				new String[] { String.valueOf(id) }, null, null, null);
 
 		if (!c.moveToFirst()) {
@@ -74,14 +84,20 @@ public class ViewEpisodeActivity extends BaseActivity {
 			return;
 		}
 
-		long podcastId = c.getLong(c.getColumnIndex(DatabaseHelper.Episode.PODCAST));
+		long podcastId = c.getLong(c
+				.getColumnIndex(DatabaseHelper.Episode.PODCAST));
 		episodeTitle.setText(c.getString(c
 				.getColumnIndex(DatabaseHelper.Episode.TITLE)));
-		String description = Utils.normalizeString(c.getString(c
+		descriptionText = Utils.normalizeString(c.getString(c
 				.getColumnIndex(DatabaseHelper.Episode.DESCRIPTION)));
-		Log.d(getClass().getSimpleName(),
-				"description length: " + description.length());
-		episodeDescription.setText(Html.fromHtml(description));
+		if (imageUrlMap == null) {
+			episodeDescription.setText(Html.fromHtml(descriptionText,
+					new ImageGetterPrefetch(), null));
+			new ImagePrefetchTask().execute();
+		} else {
+			episodeDescription.setText(Html.fromHtml(descriptionText,
+					new ImageGetter(), null));
+		}
 
 		c.close();
 
@@ -93,8 +109,8 @@ public class ViewEpisodeActivity extends BaseActivity {
 		}
 
 		// Update podcast information
-		c = dbHelper.getReadableDatabase().query(DatabaseHelper.Podcast._TABLE, null,
-				String.format("%s = ?", DatabaseHelper.Podcast.ID),
+		c = dbHelper.getReadableDatabase().query(DatabaseHelper.Podcast._TABLE,
+				null, String.format("%s = ?", DatabaseHelper.Podcast.ID),
 				new String[] { String.valueOf(podcastId) }, null, null, null,
 				"1");
 
@@ -116,6 +132,75 @@ public class ViewEpisodeActivity extends BaseActivity {
 	protected void onDestroy() {
 		// TODO Auto-generated method stub
 		super.onDestroy();
+	}
+
+	public Drawable getDrawable(String src) {
+		return null;
+	}
+
+	private Queue<String> imageUrlQueue;
+	private Map<String, String> imageUrlMap;
+
+	private class ImageGetterPrefetch implements Html.ImageGetter {
+
+		public ImageGetterPrefetch() {
+			imageUrlQueue = new LinkedList<String>();
+		}
+
+		public Drawable getDrawable(String source) {
+			imageUrlQueue.offer(source);
+			return null;
+		}
+
+	}
+
+	private class ImagePrefetchTask extends AsyncTask<Void, String, Void> {
+
+		@Override
+		protected void onPreExecute() {
+			imageUrlMap = new HashMap<String, String>();
+		}
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			DescriptionImageDownloader did = new DescriptionImageDownloader(
+					ViewEpisodeActivity.this);
+			String url;
+			while ((url = imageUrlQueue.poll()) != null) {
+				try {
+					imageUrlMap.put(url, did.fetchImage(url));
+				} catch (Exception e) {
+					// Who cares?
+				}
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			episodeDescription.setText(Html.fromHtml(descriptionText,
+					new ImageGetter(), null));
+		}
+
+	}
+
+	private class ImageGetter implements Html.ImageGetter {
+
+		// TODO: Fix it!
+		private final float SCALE = 1.4F;
+
+		public Drawable getDrawable(String source) {
+			String file = imageUrlMap.get(source);
+			if (file == null) {
+				return null;
+			}
+			Log.d(getClass().getName(), source + " => " + file);
+			Drawable d = new BitmapDrawable(getResources(), file);
+			d.setBounds(0, 0, (int) (d.getIntrinsicWidth() * SCALE),
+					(int) (d.getIntrinsicHeight() * SCALE));
+			return d;
+		}
+
 	}
 
 }
