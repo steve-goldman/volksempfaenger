1aed4ad00feb77d4d90013dfd9800883a2c1a113
Julian Brost <julian.brost@googlemail.com>
2011-08-25 05:41:28 +0200

    DownloadService: Implemented concurrent download limit.

--- src/net/x4a42/volksempfaenger/PreferenceKeys.java
+++ src/net/x4a42/volksempfaenger/PreferenceKeys.java
@@ -5,6 +5,7 @@ public final class PreferenceKeys {
 	public static final String DOWNLOAD_AUTO = "download_auto";
 	public static final String DOWNLOAD_WIFI = "download_wifi";
 	public static final String DOWNLOAD_CHARGING = "download_charging";
+	public static final String DOWNLOAD_CONCURRENT = "download_concurrent";
 	public static final String STORAGE_LOCATION = "storage_location";
 	public static final String ABOUT_VERSION = "about_version";
 	public static final String ABOUT_WEBSITE = "about_website";
