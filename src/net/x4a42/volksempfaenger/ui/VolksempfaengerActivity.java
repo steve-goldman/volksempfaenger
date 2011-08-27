cc85958946cadd555758dcea3f32c1d82313ea32
Julian Brost <julian.brost@googlemail.com>
2011-08-27 22:56:34 +0200

    Moved some stuff from VolksempfaengerActivity to a new DebugActivity.

--- src/net/x4a42/volksempfaenger/ui/VolksempfaengerActivity.java
+++ src/net/x4a42/volksempfaenger/ui/VolksempfaengerActivity.java
@@ -1,39 +1,21 @@
 package net.x4a42.volksempfaenger.ui;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 import net.x4a42.volksempfaenger.R;
-import net.x4a42.volksempfaenger.feedparser.Enclosure;
-import net.x4a42.volksempfaenger.feedparser.Feed;
-import net.x4a42.volksempfaenger.feedparser.FeedItem;
-import net.x4a42.volksempfaenger.feedparser.FeedParser;
-import net.x4a42.volksempfaenger.feedparser.FeedParserException;
-import net.x4a42.volksempfaenger.service.CleanCacheService;
-import net.x4a42.volksempfaenger.service.DownloadService;
-import net.x4a42.volksempfaenger.service.UpdateService;
 import android.content.Intent;
-import android.content.res.Resources.NotFoundException;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class VolksempfaengerActivity extends BaseActivity implements
 		OnClickListener {
+
 	private Button buttonSubscriptionList;
-	private Button buttonListenQueue;
+	private Button buttonListenManager;
 	private Button buttonDownloadQueue;
-	private Button buttonTestFeed;
-	private Button buttonViewEpisode;
-	private Button buttonStartUpdate;
-	private Button buttonStartClean;
-	private Button buttonStartDownload;
-	private Button buttonPlayer;
+	private Button buttonDebug;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -41,22 +23,14 @@ public class VolksempfaengerActivity extends BaseActivity implements
 		setContentView(R.layout.main);
 
 		buttonSubscriptionList = (Button) findViewById(R.id.button_subscriptionlist);
-		buttonListenQueue = (Button) findViewById(R.id.button_listenqueue);
-		buttonDownloadQueue = (Button) findViewById(R.id.button_downloadqueue);
-		buttonTestFeed = (Button) findViewById(R.id.button_testfeed);
-		buttonViewEpisode = (Button) findViewById(R.id.button_viewepisode);
-		buttonStartUpdate = (Button) findViewById(R.id.button_startupdate);
-		buttonStartClean = (Button) findViewById(R.id.button_startclean);
-		buttonStartDownload = (Button) findViewById(R.id.button_startdownload);
+		buttonDownloadQueue = (Button) findViewById(R.id.button_downloadmanager);
+		buttonListenManager = (Button) findViewById(R.id.button_listenqueue);
+		buttonDebug = (Button) findViewById(R.id.button_debug);
 
 		buttonSubscriptionList.setOnClickListener(this);
-		buttonListenQueue.setOnClickListener(this);
 		buttonDownloadQueue.setOnClickListener(this);
-		buttonTestFeed.setOnClickListener(this);
-		buttonViewEpisode.setOnClickListener(this);
-		buttonStartUpdate.setOnClickListener(this);
-		buttonStartClean.setOnClickListener(this);
-		buttonStartDownload.setOnClickListener(this);
+		buttonListenManager.setOnClickListener(this);
+		buttonDebug.setOnClickListener(this);
 	}
 
 	public void onClick(View v) {
@@ -67,33 +41,17 @@ public class VolksempfaengerActivity extends BaseActivity implements
 			intent = new Intent(this, SubscriptionListActivity.class);
 			startActivity(intent);
 			return;
+		case R.id.button_downloadmanager:
+			intent = new Intent(this, DownloadListActivity.class);
+			startActivity(intent);
+			return;
 		case R.id.button_listenqueue:
 			intent = new Intent(this, ListenQueueActivity.class);
 			startActivity(intent);
 			return;
-		case R.id.button_downloadqueue:
-			intent = new Intent(this, DownloadListActivity.class);
+		case R.id.button_debug:
+			intent = new Intent(this, DebugActivity.class);
 			startActivity(intent);
-			return;
-		case R.id.button_testfeed:
-			Toast.makeText(this, "Read the logcat", Toast.LENGTH_SHORT).show();
-			testFeedParser();
-			return;
-		case R.id.button_viewepisode:
-			intent = new Intent(this, ViewEpisodeActivity.class);
-			startActivity(intent);
-			return;
-		case R.id.button_startupdate:
-			intent = new Intent(this, UpdateService.class);
-			startService(intent);
-			return;
-		case R.id.button_startclean:
-			intent = new Intent(this, CleanCacheService.class);
-			startService(intent);
-			return;
-		case R.id.button_startdownload:
-			intent = new Intent(this, DownloadService.class);
-			startService(intent);
 			return;
 		}
 	}
@@ -113,59 +71,4 @@ public class VolksempfaengerActivity extends BaseActivity implements
 		return true;
 	}
 
-	public void testFeedParser() {
-		try {
-			Feed feed = FeedParser.parse(new InputStreamReader(getResources()
-					.openRawResource(R.raw.atom_test)));
-			Log.d(TAG, "Title: " + feed.getTitle());
-			if (feed.getUrl() != null) {
-				Log.d(TAG, "URL: " + feed.getUrl());
-			}
-			if (feed.getWebsite() != null) {
-				Log.d(TAG, "Website: " + feed.getWebsite());
-			}
-			if (feed.getDescription() != null) {
-				Log.d(TAG, "Description: " + feed.getDescription());
-			}
-
-			for (FeedItem item : feed.getItems()) {
-				Log.d(TAG, "Item title: " + item.getTitle());
-				Log.d(TAG, "Item ID: " + item.getItemId());
-				if (item.getDate() != null) {
-					Log.d(TAG, item.getDate().toString());
-				}
-				if (item.getUrl() != null) {
-					Log.d(TAG, item.getUrl());
-				}
-				if (item.getDescription() != null) {
-					Log.d(TAG, item.getDescription());
-				}
-				for (Enclosure enc : item.getEnclosures()) {
-					Log.d(TAG, "Enclosure");
-					if (enc.getTitle() != null) {
-						Log.d(TAG, enc.getTitle());
-					}
-					if (enc.getUrl() != null) {
-						Log.d(TAG, enc.getUrl());
-					}
-					if (enc.getMime() != null) {
-						Log.d(TAG, enc.getMime());
-					}
-					if (enc.getSize() != 0) {
-						Log.d(TAG,
-								"Size: " + (new Long(enc.getSize())).toString());
-					}
-				}
-			}
-		} catch (NotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (FeedParserException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 }
