ef3903c134492e02f6f34e76bd5a446581e726b9
Julian Brost <julian.brost@googlemail.com>
2011-08-31 01:24:23 +0200

    Some changes in EpisodeListAdapter but we need to make some database changes to get them to work.

--- src/net/x4a42/volksempfaenger/data/EpisodeListAdapter.java
+++ src/net/x4a42/volksempfaenger/data/EpisodeListAdapter.java
@@ -1,7 +1,10 @@
 package net.x4a42.volksempfaenger.data;
 
 import java.text.DateFormat;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import net.x4a42.volksempfaenger.R;
 import android.content.Context;
@@ -11,17 +14,45 @@ import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 public class EpisodeListAdapter extends SimpleCursorAdapter {
-	static final String[] FROM = { DatabaseHelper.Episode.TITLE,
+
+	static final String[] from = { DatabaseHelper.Episode.TITLE,
 			DatabaseHelper.Episode.DATE };
-	static final int[] TO = { R.id.episode_title, R.id.episode_date };
+	static final int[] to = { R.id.episode_title, R.id.episode_date };
+
+	static final Map<Integer, Integer> rowColorMap;
+	static {
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		map.put(DatabaseHelper.Enclosure.STATE_NEW, R.color.episode_title_new);
+		map.put(DatabaseHelper.Enclosure.STATE_DOWNLOAD_QUEUED,
+				R.color.episode_title_download_queued);
+		map.put(DatabaseHelper.Enclosure.STATE_DOWNLOADING,
+				R.color.episode_title_downloading);
+		map.put(DatabaseHelper.Enclosure.STATE_DOWNLOADED,
+				R.color.episode_title_downloaded);
+		map.put(DatabaseHelper.Enclosure.STATE_LISTEN_QUEUED,
+				R.color.episode_title_listen_queued);
+		map.put(DatabaseHelper.Enclosure.STATE_LISTENING,
+				R.color.episode_title_listening);
+		map.put(DatabaseHelper.Enclosure.STATE_LISTENED,
+				R.color.episode_title_listened);
+		map.put(DatabaseHelper.Enclosure.STATE_DELETED,
+				R.color.episode_title_deleted);
+		rowColorMap = Collections.unmodifiableMap(map);
+	}
 
 	public EpisodeListAdapter(Context context, Cursor cursor) {
-		super(context, R.layout.view_subscription_row, cursor, FROM, TO);
+		super(context, R.layout.view_subscription_row, cursor, from, to);
 	}
 
 	@Override
 	public void bindView(View row, Context context, Cursor cursor) {
 		super.bindView(row, context, cursor);
+
+		// int episodeState = cursor.getInt(cursor
+		// .getColumnIndex(DatabaseHelper.Enclosure.STATE));
+		// TextView episodeTitle = (TextView)
+		// row.findViewById(R.id.episode_title);
+		// episodeTitle.setTextColor(rowColorMap.get(episodeState));
 
 		Date date = new Date(cursor.getLong(cursor
 				.getColumnIndex(DatabaseHelper.Episode.DATE)) * 1000);
@@ -29,5 +60,4 @@ public class EpisodeListAdapter extends SimpleCursorAdapter {
 		TextView episodeDate = (TextView) row.findViewById(R.id.episode_date);
 		episodeDate.setText(DateFormat.getDateInstance().format(date));
 	}
-
 }
