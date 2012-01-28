5f9ba9fe05ca70a8ba4bdc915860bbde33590be5
Julian Brost <julian.brost@googlemail.com>
2012-01-28 18:22:15 +0100

    [WONT COMPILE] Implemented delete stuff in the ContentProvider (untested)

--- src/net/x4a42/volksempfaenger/data/DeleteHelper.java
+++ src/net/x4a42/volksempfaenger/data/DeleteHelper.java
@@ -8,40 +8,38 @@ public class DeleteHelper extends ContentProviderHelper {
 		super(dbHelper);
 	}
 
+	private int delete(String table, String selection, String[] selectionArgs) {
+		return getWritableDatabase().delete(table, selection, selectionArgs);
+	}
+
 	public int deletePodcastDir(Uri uri, String selection,
 			String[] selectionArgs) {
-		// TODO Auto-generated method stub
-		return 0;
+		return delete(PODCAST_TABLE, selection, selectionArgs);
 	}
 
 	public int deletePodcastItem(Uri uri, String selection,
 			String[] selectionArgs) {
-		// TODO Auto-generated method stub
-		return 0;
+		return deletePodcastDir(null, PODCAST_WHERE_ID, selectionArray(uri));
 	}
 
 	public int deleteEpisodeDir(Uri uri, String selection,
 			String[] selectionArgs) {
-		// TODO Auto-generated method stub
-		return 0;
+		return delete(EPISODE_TABLE, selection, selectionArgs);
 	}
 
 	public int deleteEpisodeItem(Uri uri, String selection,
 			String[] selectionArgs) {
-		// TODO Auto-generated method stub
-		return 0;
+		return deleteEpisodeDir(null, EPISODE_WHERE_ID, selectionArray(uri));
 	}
 
 	public int deleteEnclosureDir(Uri uri, String selection,
 			String[] selectionArgs) {
-		// TODO Auto-generated method stub
-		return 0;
+		return delete(ENCLOSURE_TABLE, selection, selectionArgs);
 	}
 
 	public int deleteEnclosureItem(Uri uri, String selection,
 			String[] selectionArgs) {
-		// TODO Auto-generated method stub
-		return 0;
+		return deleteEnclosureDir(null, ENCLOSURE_WHERE_ID, selectionArray(uri));
 	}
 
 }
