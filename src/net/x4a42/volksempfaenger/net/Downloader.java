cd20028131f45116b7555b1493cf57adf522b303
Julian Brost <julian.brost@googlemail.com>
2012-02-19 17:32:13 +0100

    Added HttpResponseCache to the base Downloader class.

--- src/net/x4a42/volksempfaenger/net/Downloader.java
+++ src/net/x4a42/volksempfaenger/net/Downloader.java
@@ -1,19 +1,49 @@
 package net.x4a42.volksempfaenger.net;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 import net.x4a42.volksempfaenger.R;
+import net.x4a42.volksempfaenger.Utils;
 import net.x4a42.volksempfaenger.VolksempfaengerApplication;
 import android.content.Context;
+import android.net.http.HttpResponseCache;
+import android.os.Build;
+import android.util.Log;
 
 public abstract class Downloader {
 
+	public static final String TAG = "Downloader";
+
 	private Context context;
+	private static HttpResponseCache cache;
 
 	public Downloader(Context context) {
 		this.context = context;
+		initCache();
+	}
+
+	private void initCache() {
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB_MR2)
+			return;
+		if (cache != null)
+			return;
+		synchronized (Downloader.class) {
+			if (cache != null)
+				return;
+			cache = HttpResponseCache.getInstalled();
+			if (cache != null)
+				return;
+			try {
+				File path = Utils.joinPath(context.getExternalCacheDir(),
+						"http");
+				HttpResponseCache.install(path, 16 * 1024 * 1024 /* 16 MiB */);
+			} catch (Exception e) {
+				Log.i(TAG, "Could not install HttpResponseCache", e);
+			}
+		}
 	}
 
 	protected Context getContext() {
