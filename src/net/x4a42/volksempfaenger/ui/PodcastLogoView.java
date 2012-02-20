c3e190531f2be2d1c0b6d4e6832c85165478f5fe
Julian Brost <julian.brost@googlemail.com>
2012-02-20 17:29:15 +0100

    Use an AsyncTask in PodcastLogoView to load the image and replaced the default image.

--- src/net/x4a42/volksempfaenger/ui/PodcastLogoView.java
+++ src/net/x4a42/volksempfaenger/ui/PodcastLogoView.java
@@ -5,24 +5,34 @@ import net.x4a42.volksempfaenger.Utils;
 import net.x4a42.volksempfaenger.data.CacheMap;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
 public class PodcastLogoView extends ImageView {
 
+	private static CacheMap<Long, Bitmap> cache = new CacheMap<Long, Bitmap>();
+
 	private long podcastId;
-	private static CacheMap<Long, Bitmap> logoCache = new CacheMap<Long, Bitmap>();
+	private AsyncTask<Void, Void, Bitmap> lastTask;
 
 	public PodcastLogoView(Context context) {
 		super(context);
+		init();
 	}
 
 	public PodcastLogoView(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		init();
 	}
 
 	public PodcastLogoView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
+		init();
+	}
+
+	private void init() {
+		setImageResource(R.drawable.default_logo);
 	}
 
 	public void setPodcastId(long id) {
@@ -30,19 +40,57 @@ public class PodcastLogoView extends ImageView {
 			// don't reload the logo
 			return;
 		}
-		podcastId = id;
 
-		Bitmap podcastLogoBitmap = Utils.getPodcastLogoBitmap(getContext(),
-				podcastId, logoCache);
-		if (podcastLogoBitmap == null) {
-			setImageResource(R.drawable.default_logo);
-		} else {
-			setImageBitmap(podcastLogoBitmap);
+		if (lastTask != null) {
+			lastTask.cancel(false);
 		}
+
+		Bitmap logo = cache.get(id);
+		if (logo != null) {
+			// the logo is cached, we don't need to fire off an AsyncTask
+			setImageBitmap(logo);
+		} else {
+			lastTask = new LoadTask(id).execute();
+		}
+
+		podcastId = id;
 	}
 
 	public long getPodcastId() {
 		return podcastId;
+	}
+
+	private class LoadTask extends AsyncTask<Void, Void, Bitmap> {
+
+		private long id;
+
+		private LoadTask(long id) {
+			this.id = id;
+		}
+
+		@Override
+		protected Bitmap doInBackground(Void... params) {
+			Bitmap logo = Utils.getPodcastLogoBitmap(getContext(), id);
+			if (logo != null) {
+				cache.put(id, logo);
+			}
+			return logo;
+		}
+
+		@Override
+		protected void onPostExecute(Bitmap result) {
+			if (result != null) {
+				setImageBitmap(result);
+			} else {
+				setImageResource(R.drawable.default_logo);
+			}
+		}
+
+		@Override
+		protected void onCancelled(Bitmap result) {
+			init();
+		}
+
 	}
 
 }
