d931700cbf38f6b34f6a6962f6de321a7c233a9c
Julian Brost <julian@0x4a42.net>
2012-07-08 22:54:45 +0200

    Fixed #3: Updated DownloadListFragment to show more details and implemented an action mode for deleting multiple downloads.

--- src/net/x4a42/volksempfaenger/Utils.java
+++ src/net/x4a42/volksempfaenger/Utils.java
@@ -258,4 +258,20 @@ public class Utils {
 		Bitmap scaled = Bitmap.createScaledBitmap(logo, size, size, true);
 		return scaled;
 	}
+
+	// taken from http://stackoverflow.com/a/5599842/731539
+	public static String readableFileSize(long size) {
+
+		if (size <= 0) {
+			return "0";
+		}
+
+		final String[] units = new String[] { "B", "KB", "MB", "GB", "TB" };
+		int digitGroups = (int) (Math.log10(size) / Math.log10(1024));
+
+		return new DecimalFormat("#,##0.0").format(size
+				/ Math.pow(1024, digitGroups))
+				+ " " + units[digitGroups];
+
+	}
 }
--- src/net/x4a42/volksempfaenger/ui/DownloadListFragment.java
+++ src/net/x4a42/volksempfaenger/ui/DownloadListFragment.java
@@ -1,9 +1,12 @@
 package net.x4a42.volksempfaenger.ui;
 
-import net.x4a42.volksempfaenger.Log;
+import java.text.DateFormat;
+
 import net.x4a42.volksempfaenger.R;
+import net.x4a42.volksempfaenger.Utils;
 import net.x4a42.volksempfaenger.data.Columns.Episode;
 import net.x4a42.volksempfaenger.data.EpisodeCursor;
+import net.x4a42.volksempfaenger.data.EpisodeHelper;
 import net.x4a42.volksempfaenger.data.VolksempfaengerContentProvider;
 import android.app.DownloadManager;
 import android.app.ListFragment;
@@ -14,9 +17,15 @@ import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView.MultiChoiceModeListener;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
@@ -29,7 +38,6 @@ public class DownloadListFragment extends ListFragment implements
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		adapter = new Adapter();
-
 	}
 
 	@Override
@@ -39,57 +47,133 @@ public class DownloadListFragment extends ListFragment implements
 		setListShown(false);
 		setEmptyText(getText(R.string.message_no_downloads));
 		getLoaderManager().initLoader(0, null, this);
+		ListView listView = getListView();
+		listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+		listView.setMultiChoiceModeListener(mMultiChoiceModeListener);
 	}
 
 	private class Adapter extends SimpleCursorAdapter {
 		public Adapter() {
-			super(getActivity(), R.layout.download_list_row, null,
-					new String[] { Episode.TITLE },
-					new int[] { R.id.download_title }, 0);
+			super(getActivity(), R.layout.download_list_row, null, new String[] {
+					Episode.TITLE, Episode.PODCAST_TITLE }, new int[] {
+					R.id.episode_title, R.id.podcast_title }, 0);
 		}
 
 		@Override
 		public void bindView(View view, Context context, Cursor cursor) {
 			super.bindView(view, context, cursor);
+
 			EpisodeCursor episodeCursor = (EpisodeCursor) cursor;
+
+			int status = episodeCursor.getDownloadStatus();
+			long done = episodeCursor.getDownloadDone();
+			long total = episodeCursor.getDownloadTotal();
+
+			// percentage
+			TextView percentage = (TextView) view.findViewById(R.id.percentage);
+			if (status == DownloadManager.STATUS_RUNNING
+					|| status == DownloadManager.STATUS_PAUSED) {
+				percentage.setVisibility(View.VISIBLE);
+				percentage.setText(String.format("%.0f%%", (double) done
+						/ total * 100));
+			} else {
+				percentage.setVisibility(View.GONE);
+			}
+
+			// progress text
+			TextView progressText = (TextView) view
+					.findViewById(R.id.progress_text);
+			if (status == DownloadManager.STATUS_RUNNING
+					|| status == DownloadManager.STATUS_PAUSED) {
+				progressText.setVisibility(View.VISIBLE);
+				progressText.setText(String.format("%s / %s",
+						Utils.readableFileSize(done),
+						Utils.readableFileSize(total)));
+			} else if (status == DownloadManager.STATUS_SUCCESSFUL) {
+				progressText.setVisibility(View.VISIBLE);
+				progressText.setText(Utils.readableFileSize(done));
+			} else {
+				progressText.setVisibility(View.GONE);
+			}
+
+			// progress bar
 			ProgressBar progressBar = (ProgressBar) view
-					.findViewById(R.id.download_progress_bar);
-			TextView statusText = (TextView) view
-					.findViewById(R.id.download_status);
-			if (episodeCursor.getDownloadStatus() == DownloadManager.STATUS_RUNNING) {
+					.findViewById(R.id.progress_bar);
+			if (status == DownloadManager.STATUS_PENDING
+					|| status == DownloadManager.STATUS_PAUSED) {
 				progressBar.setVisibility(View.VISIBLE);
-				statusText.setVisibility(View.GONE);
-				long done = episodeCursor.getDownloadDone();
-				long total = episodeCursor.getDownloadTotal();
+				progressBar.setIndeterminate(true);
+			} else if (status == DownloadManager.STATUS_RUNNING) {
+				int progress;
+				int progressMax;
+
 				if (done > Integer.MAX_VALUE || total > Integer.MAX_VALUE) {
-					Log.wtf(this, "Size > Integer.MAX_VALUE not yet supported"); // TODO
-					return;
+					if (done > total) {
+						// this shouldn't happen but we are prepared
+						progress = Integer.MAX_VALUE;
+					} else {
+						progress = (int) ((double) done / Long.MAX_VALUE * Integer.MAX_VALUE);
+					}
+					progressMax = Integer.MAX_VALUE;
+				} else {
+					progress = (int) done;
+					progressMax = (int) total;
 				}
-				progressBar.setMax((int) total);
-				progressBar.setProgress((int) done);
+
+				progressBar.setVisibility(View.VISIBLE);
+				progressBar.setIndeterminate(false);
+				progressBar.setMax(progressMax);
+				progressBar.setProgress(progress);
 			} else {
 				progressBar.setVisibility(View.GONE);
-				statusText.setVisibility(View.VISIBLE);
-				String text;
-				switch (episodeCursor.getDownloadStatus()) {
-				case DownloadManager.STATUS_FAILED:
-					text = getString(R.string.download_status_failed);
-					break;
-				case DownloadManager.STATUS_PAUSED:
-					text = getString(R.string.download_status_paused);
-					break;
-				case DownloadManager.STATUS_PENDING:
-					text = getString(R.string.download_status_pending);
-					break;
-				case DownloadManager.STATUS_SUCCESSFUL:
-					text = getString(R.string.download_status_successful);
-					break;
-				default:
-					text = "Invalid status";
-				}
-				statusText.setText(text);
-
 			}
+
+			// additional info
+			TextView additionalInfo = (TextView) view
+					.findViewById(R.id.additional_info);
+			if (status == DownloadManager.STATUS_SUCCESSFUL) {
+				additionalInfo.setVisibility(View.VISIBLE);
+				additionalInfo.setText(DateFormat.getDateInstance().format(
+						episodeCursor.getDate() * 1000));
+			} else if (status == DownloadManager.STATUS_FAILED) {
+				additionalInfo.setVisibility(View.VISIBLE);
+				// TODO show error message
+				additionalInfo.setText("Download failed");
+			} else {
+				additionalInfo.setVisibility(View.GONE);
+			}
+
+			// episode duration
+			TextView episodeDuration = (TextView) view
+					.findViewById(R.id.episode_duration);
+			if (status == DownloadManager.STATUS_SUCCESSFUL) {
+				episodeDuration.setVisibility(View.VISIBLE);
+				episodeDuration.setText(Utils.formatTime(episodeCursor
+						.getDurationTotal()));
+			} else {
+				episodeDuration.setVisibility(View.GONE);
+			}
+
+			// download error
+			View downloadError = view.findViewById(R.id.download_error);
+
+			if (status == DownloadManager.STATUS_FAILED) {
+				downloadError.setVisibility(View.VISIBLE);
+
+			} else {
+				downloadError.setVisibility(View.GONE);
+			}
+
+			// change layout based on visibility of the download error icon
+			View episodeTitle = view.findViewById(R.id.episode_title);
+			RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) episodeTitle
+					.getLayoutParams();
+			layoutParams.addRule(RelativeLayout.LEFT_OF, downloadError
+					.getVisibility() == View.VISIBLE ? downloadError.getId()
+					: percentage.getId());
+			episodeTitle.setLayoutParams(layoutParams);
+
+			// podcast logo
 			PodcastLogoView logoView = (PodcastLogoView) view
 					.findViewById(R.id.podcast_logo);
 			logoView.setPodcastId(episodeCursor.getPodcastId());
@@ -101,10 +185,13 @@ public class DownloadListFragment extends ListFragment implements
 		return new CursorLoader(getActivity(),
 				VolksempfaengerContentProvider.EPISODE_URI, new String[] {
 						Episode.PODCAST_ID, Episode._ID, Episode.TITLE,
-						Episode.DOWNLOAD_ID, Episode.DOWNLOAD_STATUS,
+						Episode.DATE, Episode.PODCAST_TITLE,
+						Episode.DOWNLOAD_ID, Episode.DURATION_TOTAL,
+						Episode.DOWNLOAD_STATUS,
 						Episode.DOWNLOAD_BYTES_DOWNLOADED_SO_FAR,
 						Episode.DOWNLOAD_TOTAL_SIZE_BYTES },
-				Episode.DOWNLOAD_ID + " != 0", null, "download.status ASC");
+				Episode.DOWNLOAD_ID + " != 0", null,
+				"download.status ASC, download._id DESC");
 	}
 
 	@Override
@@ -128,5 +215,50 @@ public class DownloadListFragment extends ListFragment implements
 		intent.putExtra("id", id);
 		startActivity(intent);
 	}
+
+	/* Action Mode */
+
+	private MultiChoiceModeListener mMultiChoiceModeListener = new MultiChoiceModeListener() {
+
+		@Override
+		public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+			MenuInflater inflater = mode.getMenuInflater();
+			inflater.inflate(R.menu.action_downloads, menu);
+			return true;
+		}
+
+		@Override
+		public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+			return false;
+		}
+
+		@Override
+		public void onItemCheckedStateChanged(ActionMode mode, int position,
+				long id, boolean checked) {
+			return;
+		}
+
+		@Override
+		public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+			switch (item.getItemId()) {
+			case R.id.item_delete:
+				EpisodeHelper.deleteDownload(
+						getActivity().getContentResolver(),
+						(DownloadManager) getActivity().getSystemService(
+								Context.DOWNLOAD_SERVICE), getListView()
+								.getCheckedItemIds());
+				return true;
+
+			default:
+				return false;
+			}
+		}
+
+		@Override
+		public void onDestroyActionMode(ActionMode mode) {
+			return;
+		}
+
+	};
 
 }
