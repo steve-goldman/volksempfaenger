03ed39c653b6d964ea51e015e80ade2b64012a57
Julian Brost <julian@0x4a42.net>
2012-07-08 22:54:45 +0200

    Fixed #3: Updated DownloadListFragment to show more details and implemented an action mode for deleting multiple downloads.

---
+++ src/net/x4a42/volksempfaenger/Utils.java
@@ -0,0 +1,277 @@
+package net.x4a42.volksempfaenger;
+
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Map;
+
+import net.x4a42.volksempfaenger.misc.BitmapCache;
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.preference.PreferenceManager;
+
+public class Utils {
+	public static String joinArray(Object[] objects, CharSequence sep) {
+		if (objects == null) {
+			return null;
+		}
+
+		StringBuilder buf = new StringBuilder();
+
+		int i = 0;
+		while (i < objects.length) {
+			if (objects[i] != null) {
+				buf.append(objects[i]);
+			}
+			if (++i < objects.length) {
+				buf.append(sep);
+			}
+		}
+
+		return buf.toString();
+	}
+
+	public static String joinArray(long[] longs, CharSequence sep) {
+		if (longs == null) {
+			return null;
+		}
+
+		String[] objects = new String[longs.length];
+
+		for (int i = 0; i < longs.length; i++) {
+			objects[i] = String.valueOf(longs[i]);
+		}
+
+		return joinArray(objects, sep);
+	}
+
+	public static long toUnixTimestamp(Date date) {
+		return date.getTime() / 1000L;
+	}
+
+	public static String normalizeString(String string) {
+		return string.replaceAll("\\s+", " ");
+	}
+
+	public static String normalizeFilename(String filename) {
+		return filename.replaceAll("[^A-Za-z0-9-_\\.]+", "_");
+	}
+
+	public static File joinPath(File base, String... children) {
+		for (String child : children) {
+			base = new File(base, child);
+		}
+		return base;
+	}
+
+	public static File joinPath(String base, String... children) {
+		return joinPath(new File(base), children);
+	}
+
+	public static File getPodcastLogoFile(Context context, long podcastId) {
+		return Utils.joinPath(context.getExternalFilesDir(null), "logos",
+				String.valueOf(podcastId));
+	}
+
+	public static Bitmap getPodcastLogoBitmap(Context context, long podcastId) {
+		final BitmapCache cache = VolksempfaengerApplication.getCache();
+		Bitmap bitmap = cache.get(podcastId);
+		if (bitmap == null) {
+			File podcastLogoFile = getPodcastLogoFile(context, podcastId);
+			if (podcastLogoFile.isFile()) {
+				Bitmap tempBitmap = BitmapFactory.decodeFile(podcastLogoFile
+						.getAbsolutePath());
+				bitmap = scalePodcastLogo(context, tempBitmap);
+				if (tempBitmap != bitmap) {
+					tempBitmap.recycle();
+				}
+				cache.put(podcastId, bitmap);
+				Map<Long, Bitmap> previewCache = VolksempfaengerApplication
+						.getPreviewCache();
+				if (previewCache.get(podcastId) == null) {
+					Bitmap preview = scalePodcastLogoToSize(bitmap,
+							Constants.PREVIEW_LOGO_SIZE);
+					previewCache.put(podcastId, preview);
+				}
+			} else {
+				return null;
+			}
+		}
+		return bitmap;
+	}
+
+	public static File getDescriptionImageFile(Context context, String url) {
+		return Utils.joinPath(context.getExternalCacheDir(), "images",
+				sha1.hash(url));
+	}
+
+	public static File getEnclosureFile(Context context, long enclosureId,
+			String filename) {
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(context);
+		String location = prefs.getString(PreferenceKeys.STORAGE_LOCATION,
+				VolksempfaengerApplication.getDefaultStorageLocation());
+		String fullFilename;
+		if (filename == null) {
+			fullFilename = String.valueOf(enclosureId);
+		} else {
+			fullFilename = new StringBuffer().append(enclosureId).append('_')
+					.append(normalizeFilename(filename)).toString();
+		}
+		return new File(location, fullFilename);
+	}
+
+	public static class sha1 {
+		// http://stackoverflow.com/questions/5980658/how-to-sha1-hash-a-string-in-android/5980789#5980789
+		private static String convertToHex(byte[] data) {
+			StringBuffer buf = new StringBuffer();
+			for (int i = 0; i < data.length; i++) {
+				int halfbyte = (data[i] >>> 4) & 0x0F;
+				int two_halfs = 0;
+				do {
+					if ((0 <= halfbyte) && (halfbyte <= 9))
+						buf.append((char) ('0' + halfbyte));
+					else
+						buf.append((char) ('a' + (halfbyte - 10)));
+					halfbyte = data[i] & 0x0F;
+				} while (two_halfs++ < 1);
+			}
+			return buf.toString();
+		}
+
+		public static String hash(String text) {
+			MessageDigest md;
+			try {
+				md = MessageDigest.getInstance("SHA-1");
+			} catch (NoSuchAlgorithmException e) {
+				Log.wtf(Log.getTag(Utils.class), e);
+				return null;
+			}
+			byte[] sha1hash = new byte[40];
+			try {
+				md.update(text.getBytes("utf8"), 0, text.length());
+			} catch (UnsupportedEncodingException e) {
+				Log.w(Log.getTag(Utils.class), e);
+				try {
+					md.update(text.getBytes("iso-8859-1"), 0, text.length());
+				} catch (UnsupportedEncodingException e1) {
+					Log.wtf(Log.getTag(Utils.class), e);
+					return null;
+				}
+			}
+			sha1hash = md.digest();
+			return convertToHex(sha1hash);
+		}
+	}
+
+	public static boolean stringBoolean(String str) {
+		return str.equals("true") || str.equals("yes");
+	}
+
+	public static String filenameFromUrl(String url) {
+		int slashIndex = url.lastIndexOf("/");
+		String filename = null;
+		if (slashIndex != -1) {
+			filename = url.substring(slashIndex + 1);
+			if (filename.length() == 0) {
+				filename = null;
+			}
+		}
+		return filename;
+	}
+
+	public static void internStringArray(String[] strings) {
+		for (int i = 0; i < strings.length; i++) {
+			if (strings[i] != null) {
+				strings[i] = strings[i].intern();
+			}
+		}
+	}
+
+	public static String hashContentValues(ContentValues values) {
+		ArrayList<String> keys = new ArrayList<String>(values.keySet());
+		Collections.sort(keys);
+		StringBuilder b = new StringBuilder();
+		for (String key : keys) {
+			b.append(key);
+			b.append((char) 0);
+			b.append(values.getAsString(key));
+			b.append((char) 0);
+		}
+		return sha1.hash(b.toString());
+	}
+
+	public static String formatTime(int milliseconds) {
+		int seconds = milliseconds / 1000;
+		int hours = seconds / 3600;
+		int minutes = (seconds / 60) - (hours * 60);
+		int seconds2 = seconds - (minutes * 60) - (hours * 3600);
+		DecimalFormat format = new DecimalFormat("00");
+		return format.format(hours) + ":" + format.format(minutes) + ":"
+				+ format.format(seconds2);
+	}
+
+	public static String trimmedString(StringBuilder stringBuilder) {
+		final int length = stringBuilder.length();
+		int start, end;
+		for (start = 0; start < length; start++) {
+			if (!Character.isWhitespace(stringBuilder.charAt(start))) {
+				break;
+			}
+		}
+		for (end = length - 1; end >= start; end--) {
+			if (!Character.isWhitespace(stringBuilder.charAt(end))) {
+				break;
+			}
+		}
+		return stringBuilder.substring(start, end + 1);
+	}
+
+	@TargetApi(16)
+	@SuppressWarnings("deprecation")
+	public static Notification notificationFromBuilder(Notification.Builder nb) {
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+			return nb.getNotification();
+		} else {
+			return nb.build();
+		}
+	}
+
+	public static Bitmap scalePodcastLogo(Context context, Bitmap logo) {
+		int maxSize = 2 * context.getResources().getDimensionPixelSize(
+				R.dimen.grid_column_width);
+		return scalePodcastLogoToSize(logo, maxSize);
+	}
+
+	private static Bitmap scalePodcastLogoToSize(Bitmap logo, int size) {
+		Bitmap scaled = Bitmap.createScaledBitmap(logo, size, size, true);
+		return scaled;
+	}
+
+	// taken from http://stackoverflow.com/a/5599842/731539
+	public static String readableFileSize(long size) {
+
+		if (size <= 0) {
+			return "0";
+		}
+
+		final String[] units = new String[] { "B", "KB", "MB", "GB", "TB" };
+		int digitGroups = (int) (Math.log10(size) / Math.log10(1024));
+
+		return new DecimalFormat("#,##0.0").format(size
+				/ Math.pow(1024, digitGroups))
+				+ " " + units[digitGroups];
+
+	}
+}
---
+++ src/net/x4a42/volksempfaenger/ui/NowPlayingFragment.java
@@ -0,0 +1,303 @@
+package net.x4a42.volksempfaenger.ui;
+
+import net.x4a42.volksempfaenger.R;
+import net.x4a42.volksempfaenger.Utils;
+import net.x4a42.volksempfaenger.data.Columns.Episode;
+import net.x4a42.volksempfaenger.data.EpisodeCursor;
+import net.x4a42.volksempfaenger.service.PlaybackHelper.Event;
+import net.x4a42.volksempfaenger.service.PlaybackHelper.EventListener;
+import net.x4a42.volksempfaenger.service.PlaybackService;
+import net.x4a42.volksempfaenger.service.PlaybackService.PlaybackBinder;
+import net.x4a42.volksempfaenger.service.PlaybackService.PlaybackRemote;
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+public class NowPlayingFragment extends Fragment implements ServiceConnection,
+		OnClickListener, OnSeekBarChangeListener, EventListener {
+
+	private Uri episodeUri;
+	private boolean isPlaying;
+
+	private LinearLayout info;
+	private PodcastLogoView logo;
+	private LinearLayout episodeInfo;
+	private TextView episode;
+	private TextView podcast;
+	private ImageButton infoPause;
+	private SeekBar seekbar;
+	private LinearLayout controls;
+	private TextView position;
+	private ImageButton back;
+	private ImageButton pause;
+	private ImageButton forward;
+	private TextView duration;
+
+	private PlaybackRemote remote;
+	private Handler updateHandler;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		updateHandler = new Handler();
+		onServiceDisconnected(null);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.nowplaying, container, false);
+
+		info = (LinearLayout) view.findViewById(R.id.info);
+		logo = (PodcastLogoView) view.findViewById(R.id.logo);
+		episodeInfo = (LinearLayout) view.findViewById(R.id.episode_info);
+		episode = (TextView) view.findViewById(R.id.episode);
+		podcast = (TextView) view.findViewById(R.id.podcast);
+		infoPause = (ImageButton) view.findViewById(R.id.info_pause);
+		seekbar = (SeekBar) view.findViewById(R.id.seekbar);
+		controls = (LinearLayout) view.findViewById(R.id.controls);
+		position = (TextView) view.findViewById(R.id.position);
+		back = (ImageButton) view.findViewById(R.id.back);
+		pause = (ImageButton) view.findViewById(R.id.pause);
+		forward = (ImageButton) view.findViewById(R.id.forward);
+		duration = (TextView) view.findViewById(R.id.duration);
+
+		episodeInfo.setOnClickListener(this);
+		infoPause.setOnClickListener(this);
+		seekbar.setEnabled(true);
+		seekbar.setOnSeekBarChangeListener(this);
+		back.setOnClickListener(this);
+		pause.setOnClickListener(this);
+		forward.setOnClickListener(this);
+
+		return view;
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		if (remote == null) {
+			onServiceDisconnected(null);
+		} else {
+			startUpdater();
+			remote.registerEventListener(this);
+			onPlaybackEvent(null);
+		}
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+
+		stopUpdater();
+
+		if (remote != null) {
+			remote.unregisterEventListener(this);
+		}
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+
+		if (remote != null) {
+			getActivity().unbindService(this);
+		}
+	}
+
+	@Override
+	public void onServiceConnected(ComponentName name, IBinder binder) {
+		System.out.println(name);
+		remote = ((PlaybackBinder) binder).getRemote();
+		remote.registerEventListener(this);
+		onPlaybackEvent(null);
+	}
+
+	@Override
+	public void onServiceDisconnected(ComponentName name) {
+		remote = null;
+
+		Intent intent = new Intent(getActivity(), PlaybackService.class);
+		getActivity().startService(intent);
+		getActivity().bindService(intent, this, Activity.BIND_AUTO_CREATE);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.episode_info:
+			onClickInfo(v);
+			break;
+		case R.id.info_pause:
+		case R.id.pause:
+			onClickPause(v);
+			break;
+		case R.id.back:
+			onClickBack(v);
+			break;
+		case R.id.forward:
+			onClickForward(v);
+			break;
+		}
+	}
+
+	public void onClickInfo(View v) {
+		Intent intent = new Intent(getActivity(), ViewEpisodeActivity.class);
+		if (remote != null) {
+			intent.setData(remote.getEpisodeUri());
+		}
+		startActivity(intent);
+	}
+
+	public void onClickPause(View v) {
+		if (remote != null) {
+			remote.pause();
+		}
+	}
+
+	public void onClickBack(View v) {
+		if (remote != null) {
+			remote.movePosition(-30000);
+		}
+	}
+
+	public void onClickForward(View v) {
+		if (remote != null) {
+			remote.movePosition(30000);
+		}
+	}
+
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress,
+			boolean fromUser) {
+		if (fromUser && remote != null) {
+			stopUpdater();
+			remote.seekTo(progress);
+			updateTime();
+			startUpdater();
+		}
+
+	}
+
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar) {
+		stopUpdater();
+	}
+
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar) {
+	}
+
+	private void hideFragment() {
+		getView().setVisibility(View.GONE);
+	}
+
+	private void showFragment() {
+		getView().setVisibility(View.VISIBLE);
+	}
+
+	private void hideExtendedControls() {
+		seekbar.setVisibility(View.GONE);
+		controls.setVisibility(View.GONE);
+		info.setVisibility(View.VISIBLE);
+	}
+
+	private void showExtendedControls(EpisodeCursor cursor) {
+		startUpdater();
+		seekbar.setMax(cursor.getDurationTotal());
+		seekbar.setProgress(cursor.getDurationListened());
+		seekbar.setMax(remote.getDuration());
+		position.setText(Utils.formatTime(cursor.getDurationListened()));
+		duration.setText(Utils.formatTime(cursor.getDurationTotal()));
+		info.setVisibility(View.GONE);
+		seekbar.setVisibility(View.VISIBLE);
+		controls.setVisibility(View.VISIBLE);
+	}
+
+	@Override
+	public void onPlaybackEvent(Event event) {
+		if (remote.isPlaying() == isPlaying && isPlaying
+				&& remote.getEpisodeUri().equals(episodeUri)) {
+			// nothing changed
+			return;
+		}
+
+		isPlaying = remote.isPlaying();
+		episodeUri = remote.getEpisodeUri();
+
+		if (isPlaying) {
+			EpisodeCursor cursor;
+			String[] projection = { Episode.TITLE, Episode.PODCAST_ID,
+					Episode.PODCAST_TITLE, Episode.DURATION_LISTENED,
+					Episode.DURATION_TOTAL };
+			cursor = new EpisodeCursor(getActivity().getContentResolver()
+					.query(episodeUri, projection, null, null, null));
+
+			if (!cursor.moveToFirst()) {
+				throw new IllegalArgumentException("Episode not found");
+			}
+
+			episode.setText(cursor.getTitle());
+			podcast.setText(cursor.getPodcastTitle());
+			logo.setPodcastId(cursor.getPodcastId());
+
+			if (getActivity() instanceof ViewEpisodeActivity) {
+				ViewEpisodeActivity activity = (ViewEpisodeActivity) getActivity();
+				Uri activityUri = activity.getUri();
+				Uri remoteUri = remote.getEpisodeUri();
+				if (activityUri != null && remoteUri != null
+						&& remoteUri.equals(activityUri)) {
+					showExtendedControls(cursor);
+				} else {
+					hideExtendedControls();
+				}
+			} else {
+				hideExtendedControls();
+			}
+			cursor.close();
+
+			showFragment();
+		} else {
+			episodeUri = null;
+			hideFragment();
+		}
+	}
+
+	private Runnable updateSliderTask = new Runnable() {
+		public void run() {
+			seekbar.setProgress(remote.getPosition());
+			updateHandler.postDelayed(this, 500);
+			updateTime();
+		}
+	};
+
+	private void startUpdater() {
+		stopUpdater();
+		updateHandler.post(updateSliderTask);
+	}
+
+	private void stopUpdater() {
+		updateHandler.removeCallbacks(updateSliderTask);
+	}
+
+	private void updateTime() {
+		position.setText(Utils.formatTime(remote.getPosition()));
+	}
+
+}
