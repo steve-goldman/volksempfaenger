5c3e92331b3fce9a7a157964e83dd2f95457d718
Vincent Sch??ler <vincent@vschuessler.org>
2012-07-12 12:52:08 +0200

    Implement ability to send error reports (see #62)

--- src/net/x4a42/volksempfaenger/service/CleanCacheService.java
+++ src/net/x4a42/volksempfaenger/service/CleanCacheService.java
@@ -1,7 +1,9 @@
 package net.x4a42.volksempfaenger.service;
 
 import java.io.File;
+import java.io.FilenameFilter;
 
+import net.x4a42.volksempfaenger.Constants;
 import android.app.Service;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -9,22 +11,31 @@ import android.os.IBinder;
 
 public class CleanCacheService extends Service {
 
-
 	private class CleanCacheTask extends AsyncTask<Void, Void, Void> {
 
 		@Override
 		protected Void doInBackground(Void... params) {
-			File cache = getExternalCacheDir();
-			File images = new File(cache, "images");
-			long minTime = System.currentTimeMillis() - 3 * 24 * 60 * 60 * 1000;
-			File[] imageFiles = images.listFiles();
-			if (imageFiles != null) {
-				for (File f : imageFiles) {
-					if (f.lastModified() < minTime) {
-						f.delete();
-					}
-				}
-			}
+			final File cacheDir = getExternalCacheDir();
+			final File images = new File(cacheDir, "images");
+			// 3 days
+			final long minTime = System.currentTimeMillis() - 3 * 24 * 60 * 60
+					* 1000;
+			final File[] imageFiles = images.listFiles();
+			CleanCacheService.removeOldFiles(imageFiles, minTime);
+			final File[] errorReportFiles = cacheDir
+					.listFiles(new FilenameFilter() {
+
+						@Override
+						public boolean accept(File dir, String filename) {
+							if (filename
+									.startsWith(Constants.ERROR_REPORT_PREFIX)) {
+								return true;
+							} else {
+								return false;
+							}
+						}
+					});
+			CleanCacheService.removeOldFiles(errorReportFiles, minTime);
 			return null;
 		}
 
@@ -44,6 +55,14 @@ public class CleanCacheService extends Service {
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
+	}
+
+	public static void removeOldFiles(File[] files, long minLastModified) {
+		for (File f : files) {
+			if (f.lastModified() < minLastModified) {
+				f.delete();
+			}
+		}
 	}
 
 }
