947acdf2e674946071030e3b461702353a76b8f5
Vincent Sch??ler <vincent@vschuessler.org>
2012-07-12 12:52:08 +0200

    Implement ability to send error reports (see #62)

--- src/net/x4a42/volksempfaenger/ui/ActivityHelper.java
+++ src/net/x4a42/volksempfaenger/ui/ActivityHelper.java
@@ -1,5 +1,12 @@
 package net.x4a42.volksempfaenger.ui;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import net.x4a42.volksempfaenger.Constants;
 import net.x4a42.volksempfaenger.R;
 import net.x4a42.volksempfaenger.VolksempfaengerApplication;
 import android.app.Activity;
@@ -8,6 +15,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
+import android.net.Uri;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -67,6 +75,43 @@ public class ActivityHelper {
 				});
 		final AlertDialog alert = builder.create();
 		return alert;
+	}
+
+	public static void sendCrashReport(Context context, Exception exception) {
+		final Intent emailIntent = new Intent(Intent.ACTION_SEND);
+		emailIntent.setType("plain/text");
+		emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL,
+				net.x4a42.volksempfaenger.Constants.FEEDBACK_TO);
+		emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
+				"Error Report");
+		final String template = context
+				.getString(R.string.template_error_report_message);
+
+		final StringWriter sw = new StringWriter();
+		exception.printStackTrace(new PrintWriter(sw));
+		final String report = exception.toString() + "\n"
+				+ exception.getMessage() + "\n" + sw.toString();
+
+		boolean appendStackTraceToMessage = false;
+		final File outputDir = context.getExternalCacheDir();
+		if (outputDir == null) {
+			appendStackTraceToMessage = true;
+		}
+		try {
+			final File reportFile = File.createTempFile(
+					Constants.ERROR_REPORT_PREFIX, ".txt", outputDir);
+			final FileWriter writer = new FileWriter(reportFile);
+			writer.write(report);
+			writer.close();
+			emailIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(reportFile));
+		} catch (IOException e) {
+			appendStackTraceToMessage = true;
+		}
+		final String message = String.format(template,
+				appendStackTraceToMessage ? report : "");
+		emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, message);
+		context.startActivity(Intent.createChooser(emailIntent,
+				context.getString(R.string.title_send_error_report)));
 	}
 
 }
