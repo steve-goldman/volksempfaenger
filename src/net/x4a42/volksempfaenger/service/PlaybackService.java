ffc20876a4950a83126a91c371c1ffeb962825f3
Julian Brost <julian@0x4a42.net>
2012-07-13 20:46:47 +0200

    Added items to the menus of View{Subscription,Episode}Activity that allow opening and sharing the podcast/episode URL, fixes #44.

---
+++ src/net/x4a42/volksempfaenger/service/PlaybackService.java
@@ -0,0 +1,417 @@
+package net.x4a42.volksempfaenger.service;
+
+import java.io.File;
+import java.io.IOException;
+
+import net.x4a42.volksempfaenger.R;
+import net.x4a42.volksempfaenger.Utils;
+import net.x4a42.volksempfaenger.data.Columns.Episode;
+import net.x4a42.volksempfaenger.data.Constants;
+import net.x4a42.volksempfaenger.data.EpisodeCursor;
+import net.x4a42.volksempfaenger.data.VolksempfaengerContentProvider;
+import net.x4a42.volksempfaenger.receiver.MediaButtonEventReceiver;
+import net.x4a42.volksempfaenger.service.PlaybackHelper.Event;
+import net.x4a42.volksempfaenger.service.PlaybackHelper.EventListener;
+import net.x4a42.volksempfaenger.ui.MainActivity;
+import net.x4a42.volksempfaenger.ui.ViewEpisodeActivity;
+import net.x4a42.volksempfaenger.ui.ViewSubscriptionActivity;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.media.AudioManager;
+import android.media.RemoteControlClient;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.Handler;
+import android.os.IBinder;
+import android.support.v4.app.TaskStackBuilder;
+import android.view.KeyEvent;
+
+public class PlaybackService extends Service implements EventListener {
+
+	public static final String ACTION_PLAY = "net.x4a42.volksempfaenger.intent.action.PLAY";
+
+	private static final int NOTIFICATION_ID = 0x59d54313;
+
+	private Uri uri;
+	private Uri uriTime;
+	private Notification notification;
+	private EpisodeCursor cursor;
+	private Handler saveHandler;
+	private PlaybackBinder binder;
+	private PlaybackRemote remote;
+	private PlaybackHelper helper;
+	private RemoteControlClient remoteControlClient;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		saveHandler = new Handler();
+		binder = new PlaybackBinder();
+		remote = new PlaybackRemote();
+		helper = new PlaybackHelper(this);
+		helper.registerEventListener(this);
+
+		ComponentName mediaButtonEventReceiver = new ComponentName(
+				getPackageName(), MediaButtonEventReceiver.class.getName());
+
+		AudioManager am = helper.getAudioManager();
+		am.registerMediaButtonEventReceiver(mediaButtonEventReceiver);
+
+		PendingIntent mediaPendingIntent = PendingIntent.getBroadcast(
+				getApplicationContext(), 0, new Intent(
+						Intent.ACTION_MEDIA_BUTTON)
+						.setComponent(mediaButtonEventReceiver), 0);
+		// create and register the remote control client
+		remoteControlClient = new RemoteControlClient(mediaPendingIntent);
+		am.registerRemoteControlClient(remoteControlClient);
+
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		String action;
+		if (intent == null) {
+			return START_STICKY;
+		} else if ((action = intent.getAction()) == null) {
+			return START_STICKY;
+		} else {
+			action = action.intern();
+		}
+
+		if (action == ACTION_PLAY) {
+
+			try {
+				playEpisode(intent.getData());
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		} else if (action == Intent.ACTION_MEDIA_BUTTON) {
+
+			KeyEvent event = (KeyEvent) intent
+					.getParcelableExtra(Intent.EXTRA_KEY_EVENT);
+
+			if (event != null) {
+				switch (event.getKeyCode()) {
+				case KeyEvent.KEYCODE_MEDIA_PLAY:
+					remote.play();
+					break;
+
+				case KeyEvent.KEYCODE_MEDIA_PAUSE:
+					remote.pause();
+					break;
+
+				case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
+					if (remote.isPlaying()) {
+						remote.pause();
+					} else {
+						remote.play();
+					}
+				}
+			}
+
+		}
+
+		return START_STICKY;
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		helper.destroy();
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return binder;
+	}
+
+	/**
+	 * Public API for PlaybackService.
+	 */
+	public class PlaybackRemote {
+		private PlaybackRemote() {
+			super();
+		}
+
+		public Uri getEpisodeUri() {
+			return uri;
+		}
+
+		public boolean isPlaying() {
+			return helper.isPlaying();
+		}
+
+		public int getPosition() {
+			return helper.getPosition();
+		}
+
+		public int getDuration() {
+			return helper.getDuration();
+		}
+
+		public void play() {
+			helper.play();
+		}
+
+		public void pause() {
+			helper.pause();
+		}
+
+		public void seekTo(int position) {
+			helper.seekTo(position);
+		}
+
+		public void movePosition(int offset) {
+			helper.movePosition(offset);
+		}
+
+		public void registerEventListener(EventListener listener) {
+			helper.registerEventListener(listener);
+		}
+
+		public void unregisterEventListener(EventListener listener) {
+			helper.unregisterEventListener(listener);
+		}
+	}
+
+	public class PlaybackBinder extends Binder {
+		private PlaybackBinder() {
+			super();
+		}
+
+		public PlaybackRemote getRemote() {
+			return remote;
+		}
+	}
+
+	/**
+	 * Helper method to start playback of an episode.
+	 * 
+	 * @param episode
+	 *            Uri of the episode to play.
+	 * @throws IllegalArgumentException
+	 * @throws IOException
+	 */
+	private void playEpisode(Uri episode) throws IllegalArgumentException,
+			IOException {
+		assert (VolksempfaengerContentProvider.getTypeMime(episode) == VolksempfaengerContentProvider.Mime.EPISODE_ITEM);
+		uri = episode;
+		uriTime = ContentUris.withAppendedId(
+				VolksempfaengerContentProvider.EPISODETIME_URI,
+				ContentUris.parseId(episode));
+		cursor = new EpisodeCursor(getContentResolver().query(
+				episode,
+				new String[] { Episode._ID, Episode.TITLE, Episode.STATUS,
+						Episode.PODCAST_ID, Episode.ENCLOSURE_ID,
+						Episode.DOWNLOAD_ID, Episode.DOWNLOAD_LOCAL_URI,
+						Episode.DURATION_LISTENED, Episode.PODCAST_TITLE },
+				null, null, null));
+
+		if (!cursor.moveToFirst()) {
+			throw new IllegalArgumentException("Episode not found");
+		}
+		File enclosureFile = cursor.getDownloadFile();
+		if (enclosureFile == null || !enclosureFile.isFile()) {
+			throw new IllegalArgumentException("Episode not found");
+		}
+		helper.open(enclosureFile.getAbsolutePath());
+		ContentValues values = new ContentValues();
+		values.put(Episode.STATUS, Constants.EPISODE_STATE_LISTENING);
+		updateEpisode(values);
+	}
+
+	/**
+	 * Helper method to update the episode in the ContentProvider.
+	 * 
+	 * @param values
+	 *            Values to update.
+	 */
+	private void updateEpisode(ContentValues values) {
+		if (uri != null) {
+			getContentResolver().update(uri, values, null, null);
+		}
+	}
+
+	public Uri getCurrentEpisode() {
+		return uri;
+	}
+
+	private void startForeground() {
+		if (notification == null) {
+			return;
+		}
+		startForeground(NOTIFICATION_ID, notification);
+	}
+
+	private void stopForeground() {
+		stopForeground(true);
+	}
+
+	private void savePosition() {
+		savePosition(helper.getPosition());
+	}
+
+	private void savePosition(int position) {
+		if (uriTime != null) {
+			ContentValues values = new ContentValues();
+			values.put(Episode.DURATION_LISTENED, position);
+			getContentResolver().update(uriTime, values, null, null);
+		}
+	}
+
+	private Runnable savePositionTask = new Runnable() {
+		public void run() {
+			savePosition();
+			saveHandler.postDelayed(this, 500);
+		}
+	};
+
+	private int getDurationListened() {
+		return cursor.getInt(cursor.getColumnIndex(Episode.DURATION_LISTENED));
+	}
+
+	@Override
+	public void onPlaybackEvent(Event event) {
+		switch (event) {
+		case PREPARED:
+			onPlayerPrepared();
+			break;
+
+		case PLAY:
+			onPlayerPlay();
+			break;
+
+		case PAUSE:
+			onPlayerPause();
+			break;
+
+		case STOP:
+			onPlayerStop();
+			break;
+
+		case END:
+			onPlayerEnd();
+			break;
+
+		case RESET:
+			onPlayerReset();
+			break;
+		default:
+			break;
+		}
+	}
+
+	private void onPlayerPlay() {
+		saveHandler.post(savePositionTask);
+		remoteControlClient
+				.setPlaybackState(RemoteControlClient.PLAYSTATE_PLAYING);
+
+	}
+
+	private void onPlayerReset() {
+		stopForeground();
+	}
+
+	private void onPlayerEnd() {
+		onPlayerPause();
+		savePosition(0);
+		remoteControlClient
+				.setPlaybackState(RemoteControlClient.PLAYSTATE_STOPPED);
+
+		cursor = null;
+		uri = null;
+		uriTime = null;
+	}
+
+	private void onPlayerStop() {
+		onPlayerPause();
+		savePosition();
+		remoteControlClient
+				.setPlaybackState(RemoteControlClient.PLAYSTATE_STOPPED);
+
+		cursor = null;
+		uri = null;
+		uriTime = null;
+	}
+
+	private void onPlayerPause() {
+		saveHandler.removeCallbacks(savePositionTask);
+		remoteControlClient
+				.setPlaybackState(RemoteControlClient.PLAYSTATE_PAUSED);
+
+		stopForeground();
+	}
+
+	private void onPlayerPrepared() {
+		notification = makeNotification();
+		startForeground();
+		helper.seekTo(getDurationListened());
+		helper.play();
+	}
+
+	/**
+	 * Helper method to create a new notification for the currently playing
+	 * episode.
+	 * 
+	 * @return The newly created Notification.
+	 */
+	private Notification makeNotification() {
+
+		// Build back stack as proposed in
+		// "Google I/O 2012 - Navigation in Android", see
+		// http://www.youtube.com/watch?v=XwGHJJYBs0Q
+		Intent intent;
+		TaskStackBuilder taskBuilder = TaskStackBuilder.create(this);
+
+		// MainActivity
+		intent = new Intent(this, MainActivity.class);
+		taskBuilder.addNextIntent(intent);
+
+		// ViewSubscriptionActivity
+		intent = new Intent(this, ViewSubscriptionActivity.class);
+		intent.setData(cursor.getPodcastUri());
+		taskBuilder.addNextIntent(intent);
+
+		// ViewEpisodeActivity
+		intent = new Intent(this, ViewEpisodeActivity.class);
+		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+		intent.setData(uri);
+		taskBuilder.addNextIntent(intent);
+
+		// Get podcast logo or use default icon
+		Bitmap podcastLogo = Utils.getPodcastLogoBitmap(this,
+				cursor.getPodcastId());
+		if (podcastLogo != null) {
+			Resources res = getResources();
+			podcastLogo = Bitmap
+					.createScaledBitmap(
+							podcastLogo,
+							res.getDimensionPixelSize(android.R.dimen.notification_large_icon_width),
+							res.getDimensionPixelSize(android.R.dimen.notification_large_icon_height),
+							false);
+		}
+
+		// Build the notification and return it
+		return Utils.notificationFromBuilder(new Notification.Builder(this)
+				.setSmallIcon(R.drawable.notification)
+				.setLargeIcon(podcastLogo).setContentTitle(cursor.getTitle())
+				.setContentText(cursor.getPodcastTitle())
+				.setContentIntent(taskBuilder.getPendingIntent(0, 0))
+				.setOngoing(true).setWhen(0));
+
+	}
+
+}
---
+++ src/net/x4a42/volksempfaenger/ui/SubscriptionGridFragment.java
@@ -0,0 +1,521 @@
+package net.x4a42.volksempfaenger.ui;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import net.x4a42.volksempfaenger.Constants;
+import net.x4a42.volksempfaenger.Log;
+import net.x4a42.volksempfaenger.R;
+import net.x4a42.volksempfaenger.Utils;
+import net.x4a42.volksempfaenger.data.Columns.Podcast;
+import net.x4a42.volksempfaenger.data.PodcastCursor;
+import net.x4a42.volksempfaenger.data.PodcastHelper;
+import net.x4a42.volksempfaenger.data.VolksempfaengerContentProvider;
+import net.x4a42.volksempfaenger.feedparser.OpmlParser;
+import net.x4a42.volksempfaenger.feedparser.SubscriptionTree;
+import net.x4a42.volksempfaenger.service.UpdateService;
+import net.x4a42.volksempfaenger.service.UpdateServiceStatus;
+import net.x4a42.volksempfaenger.service.UpdateServiceStatus.Status;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Fragment;
+import android.app.LoaderManager;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.CursorLoader;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.DialogInterface.OnMultiChoiceClickListener;
+import android.content.Intent;
+import android.content.Loader;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SubscriptionGridFragment extends Fragment implements
+		OnItemClickListener, LoaderManager.LoaderCallbacks<Cursor> {
+
+	private static final int CONTEXT_EDIT = 0;
+	private static final int CONTEXT_DELETE = 1;
+
+	private static final int PICK_FILE_REQUEST = 0;
+
+	private static final String PODCAST_ORDER = "title ASC";
+
+	private GridView grid;
+	private View loading;
+	private View empty;
+	private Adapter adapter;
+	private AdapterView.AdapterContextMenuInfo currentMenuInfo;
+
+	private boolean isUpdating = false;
+	private UpdateServiceStatus.UiReceiver updateReceiver = new UpdateReceiver();
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
+		adapter = new Adapter();
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.subscription_list, container,
+				false);
+
+		grid = (GridView) view.findViewById(R.id.grid);
+		loading = view.findViewById(R.id.loading);
+		empty = view.findViewById(R.id.empty);
+
+		grid.setOnItemClickListener(this);
+		grid.setOnCreateContextMenuListener(this);
+		grid.setAdapter(adapter);
+
+		show(GLE.LOADING);
+
+		return view;
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		updateReceiver.setActivity(getActivity());
+		getLoaderManager().initLoader(0, null, this);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		UpdateServiceStatus.registerReceiver(updateReceiver);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+
+		UpdateServiceStatus.unregisterReceiver(updateReceiver);
+	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		inflater.inflate(R.menu.subscription_list, menu);
+	}
+
+	@Override
+	public void onPrepareOptionsMenu(Menu menu) {
+		super.onPrepareOptionsMenu(menu);
+
+		MenuItem update = menu.findItem(R.id.item_update);
+		if (isUpdating) {
+			update.setActionView(R.layout.actionbar_updating);
+		} else {
+			update.setActionView(null);
+		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.item_add:
+			startActivity(new Intent(getActivity(),
+					AddSubscriptionActivity.class));
+			return true;
+		case R.id.item_update:
+			getActivity().startService(
+					new Intent(getActivity(), UpdateService.class));
+			return true;
+		case R.id.import_items:
+			Intent intent = new Intent(Constants.ACTION_OI_PICK_FILE);
+			intent.putExtra(Constants.EXTRA_OI_TITLE,
+					getString(R.string.dialog_choose_opml));
+
+			// check if any app handles the pick file intent
+			List<ResolveInfo> list = getActivity().getPackageManager()
+					.queryIntentActivities(intent,
+							PackageManager.MATCH_DEFAULT_ONLY);
+			if (list.size() > 0) {
+				startActivityForResult(intent, PICK_FILE_REQUEST);
+			} else {
+				Log.v(this, "Could not start " + Constants.ACTION_OI_PICK_FILE
+						+ " Intent");
+				new AlertDialog.Builder(getActivity())
+						.setMessage(R.string.dialog_filemanager_missing_message)
+						.setPositiveButton(R.string.install,
+								installFileManagerDialogOnClickListener)
+						.setNegativeButton(R.string.cancel,
+								installFileManagerDialogOnClickListener).show();
+			}
+
+			return true;
+		default:
+			return false;
+		}
+	}
+
+	DialogInterface.OnClickListener installFileManagerDialogOnClickListener = new DialogInterface.OnClickListener() {
+
+		@Override
+		public void onClick(DialogInterface dialog, int which) {
+			switch (which) {
+
+			case DialogInterface.BUTTON_POSITIVE:
+				Intent intent = new Intent(Intent.ACTION_VIEW);
+				try {
+					intent.setData(Constants.OI_FILEMANGER_URI_PLAY);
+					startActivity(intent);
+				} catch (ActivityNotFoundException e) {
+					Toast.makeText(getActivity(),
+							R.string.message_play_store_missing,
+							Toast.LENGTH_LONG).show();
+					intent.setData(Constants.OI_FILEMANGER_URI_HTTP);
+					startActivity(intent);
+				}
+				break;
+
+			case DialogInterface.BUTTON_NEGATIVE:
+				dialog.cancel();
+				break;
+
+			}
+		}
+	};
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+
+		TextView podcastTitle = (TextView) info.targetView
+				.findViewById(R.id.podcast_title);
+		String title = podcastTitle.getText().toString();
+		menu.setHeaderTitle(title);
+
+		menu.add(0, CONTEXT_EDIT, 0, R.string.context_edit);
+		menu.add(0, CONTEXT_DELETE, 0, R.string.context_delete);
+
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		currentMenuInfo = (AdapterView.AdapterContextMenuInfo) item
+				.getMenuInfo();
+		Intent intent;
+		switch (item.getItemId()) {
+		case CONTEXT_EDIT:
+			intent = new Intent(getActivity(), EditSubscriptionActivity.class);
+			intent.putExtra("id", currentMenuInfo.id);
+			startActivity(intent);
+			return true;
+		case CONTEXT_DELETE:
+			intent = new Intent(getActivity(), DeleteSubscriptionActivity.class);
+			intent.putExtra("id", currentMenuInfo.id);
+			startActivity(intent);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		switch (requestCode) {
+		case PICK_FILE_REQUEST:
+			if (resultCode == Activity.RESULT_OK) {
+				if (data != null) {
+					try {
+						importFile(data.getData().getPath());
+					} catch (Exception e) {
+						ActivityHelper
+								.buildErrorDialog(
+										getActivity(),
+										getString(R.string.title_import_error),
+										getString(R.string.message_error_import_unexpected),
+										e).show();
+					}
+				}
+			}
+			break;
+		}
+	}
+
+	public void onItemClick(AdapterView<?> av, View v, int pos, long id) {
+		Intent intent = new Intent(getActivity(),
+				ViewSubscriptionActivity.class);
+		intent.putExtra("id", id);
+		startActivity(intent);
+	}
+
+	private void importFile(String path) {
+		SubscriptionTree tree;
+		FileInputStream fstream;
+		try {
+			fstream = new FileInputStream(path);
+			InputStreamReader in = new InputStreamReader(fstream);
+			tree = OpmlParser.parse(new BufferedReader(in));
+			for (SubscriptionTree node : tree) {
+				if (node.isFolder()) {
+					Log.v(this,
+							String.valueOf(node.getDepth()) + " "
+									+ node.getTitle());
+				} else {
+					Log.v(this,
+							String.valueOf(node.getDepth()) + " "
+									+ node.getTitle() + " " + node.getUrl());
+				}
+			}
+		} catch (FileNotFoundException e) {
+			final String message = String.format(
+					getString(R.string.message_error_file_not_found), path);
+			ActivityHelper.buildErrorDialog(getActivity(),
+					getString(R.string.title_file_not_found), message, null)
+					.show();
+			return;
+		}
+
+		final ArrayList<SubscriptionTree> items = new ArrayList<SubscriptionTree>();
+		for (SubscriptionTree node : tree) {
+			if (!node.isFolder()) {
+				items.add(node);
+			}
+		}
+		final String itemTitles[] = new String[items.size()];
+		for (int i = 0; i < items.size(); i++) {
+			itemTitles[i] = items.get(i).getTitle();
+		}
+
+		AlertDialog.Builder ab = new AlertDialog.Builder(getActivity());
+		ab.setTitle(getString(R.string.dialog_choose_import_feeds));
+
+		final boolean checked[] = new boolean[items.size()];
+		OnMultiChoiceClickListener listener = new OnMultiChoiceClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which,
+					boolean isChecked) {
+				checked[which] = isChecked;
+			}
+		};
+		ab.setMultiChoiceItems(itemTitles, null, listener);
+		ab.setPositiveButton(getString(R.string.button_import),
+				new OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						int length = 0;
+						for (; length < items.size(); length++)
+							;
+						SubscriptionTree checkedSubscriptions[] = new SubscriptionTree[length];
+						int insertPosition = 0;
+						for (int i = 0; i < items.size(); i++) {
+							if (checked[i]) {
+								Log.v(this, itemTitles[i]);
+								Log.v(this, items.get(i).getUrl());
+								checkedSubscriptions[insertPosition] = items
+										.get(i);
+								insertPosition++;
+							}
+						}
+						new ImportTask().execute(checkedSubscriptions);
+					}
+				});
+		ab.show();
+	}
+
+	private class Adapter extends SimpleCursorAdapter {
+
+		public Adapter() {
+			super(getActivity(), R.layout.subscription_list_row, null,
+					new String[] { Podcast.TITLE },
+					new int[] { R.id.podcast_title }, 0);
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			try {
+				if (convertView != null) {
+					PodcastLogoView podcastLogo = (PodcastLogoView) convertView
+							.findViewById(R.id.podcast_logo);
+					if (podcastLogo != null) {
+						podcastLogo.reset();
+					}
+				}
+			} catch (ClassCastException e) {
+				Log.v(this, e.toString());
+			}
+			return super.getView(position, convertView, parent);
+		}
+
+		@Override
+		public void bindView(View view, Context context, Cursor c) {
+			super.bindView(view, context, c);
+			PodcastCursor cursor = (PodcastCursor) c;
+
+			TextView newEpisodesText = (TextView) view
+					.findViewById(R.id.new_episodes);
+			int newEpisodes = cursor.getNewEpisodes();
+			if (newEpisodes > 9) {
+				newEpisodesText.setText("+");
+				newEpisodesText.setVisibility(View.VISIBLE);
+			} else if (newEpisodes > 0) {
+				newEpisodesText.setText(String.valueOf(newEpisodes));
+				newEpisodesText.setVisibility(View.VISIBLE);
+			} else {
+				newEpisodesText.setVisibility(View.INVISIBLE);
+			}
+
+			PodcastLogoView podcastLogo = (PodcastLogoView) view
+					.findViewById(R.id.podcast_logo);
+			podcastLogo.setPodcastId(cursor.getId());
+		}
+	}
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		return new CursorLoader(getActivity(),
+				VolksempfaengerContentProvider.PODCAST_URI, new String[] {
+						Podcast._ID, Podcast.TITLE, Podcast.NEW_EPISODES },
+				null, null, PODCAST_ORDER);
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+		if (data.getCount() == 0) {
+			show(GLE.EMPTY);
+			adapter.swapCursor(null);
+		} else {
+			adapter.swapCursor(new PodcastCursor(data));
+			show(GLE.GRID);
+		}
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
+		adapter.swapCursor(null);
+		show(GLE.LOADING);
+	}
+
+	private enum GLE { // TODO find a meaningful name
+		GRID, LOADING, EMPTY
+	}
+
+	private void show(GLE gle) {
+		switch (gle) {
+		case GRID:
+			empty.setVisibility(View.GONE);
+			loading.setVisibility(View.GONE);
+			grid.setVisibility(View.VISIBLE);
+			break;
+
+		case LOADING:
+			grid.setVisibility(View.GONE);
+			empty.setVisibility(View.GONE);
+			loading.setVisibility(View.VISIBLE);
+			break;
+
+		case EMPTY:
+			grid.setVisibility(View.GONE);
+			loading.setVisibility(View.GONE);
+			empty.setVisibility(View.VISIBLE);
+			break;
+		}
+	}
+
+	private class UpdateReceiver extends UpdateServiceStatus.UiReceiver {
+
+		@Override
+		public void receiveUi(Status status) {
+			if (status.isUpdating()) {
+				if (!isUpdating) {
+					isUpdating = true;
+					getActivity().invalidateOptionsMenu();
+				}
+			} else {
+				if (isUpdating && status.getUri() == null) {
+					isUpdating = false;
+					getActivity().invalidateOptionsMenu();
+				}
+			}
+		}
+	}
+
+	private class ImportTask extends AsyncTask<SubscriptionTree, Void, Void> {
+		final LinkedList<String> failed = new LinkedList<String>();
+
+		@Override
+		protected Void doInBackground(SubscriptionTree... subscriptions) {
+			for (SubscriptionTree subscription : subscriptions) {
+				// TODO finer grained exception handling
+				try {
+					PodcastHelper.addFeed(getActivity(), subscription.getUrl());
+				} catch (Exception e) {
+					if (subscription == null) {
+						continue;
+					}
+					String name;
+					if (subscription.getTitle() == null) {
+						name = subscription.getUrl();
+					} else {
+						name = subscription.getTitle();
+					}
+					failed.add(name);
+
+				}
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void arg0) {
+			if (failed.size() > 0) {
+				final AlertDialog.Builder builder = new AlertDialog.Builder(
+						getActivity());
+				builder.setTitle(R.string.title_import_error)
+						.setCancelable(false)
+						.setPositiveButton(R.string.ok, new OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {
+								dialog.dismiss();
+							}
+						}).setMessage(getString(R.string.message_error_import));
+				final TextView textView = new TextView(getActivity());
+				int padding = Utils.dpToPx(getActivity(), 10);
+				textView.setPadding(padding, 0, padding, padding);
+				StringBuilder strBuilder = new StringBuilder();
+				for (String podcast : failed) {
+					strBuilder.append(podcast);
+					strBuilder.append("\n");
+				}
+				textView.setText(strBuilder.substring(0,
+						strBuilder.length() - 1));
+				builder.setView(textView);
+				final AlertDialog alert = builder.create();
+				alert.show();
+			}
+		}
+	}
+
+}
