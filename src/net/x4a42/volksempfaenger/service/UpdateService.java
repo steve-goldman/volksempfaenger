b0e0d893727d30b4a6c475946428e89b5bb73f54
Vincent Sch??ler <vincent@vschuessler.org>
2012-07-30 11:11:57 +0200

    Merge v0.4.x

---
+++ src/net/x4a42/volksempfaenger/service/UpdateService.java
@@ -0,0 +1,133 @@
+package net.x4a42.volksempfaenger.service;
+
+import net.x4a42.volksempfaenger.Log;
+import net.x4a42.volksempfaenger.data.Columns.Podcast;
+import net.x4a42.volksempfaenger.data.PodcastCursor;
+import net.x4a42.volksempfaenger.data.PodcastHelper;
+import net.x4a42.volksempfaenger.data.UpdateServiceHelper;
+import net.x4a42.volksempfaenger.data.VolksempfaengerContentProvider;
+import net.x4a42.volksempfaenger.feedparser.Feed;
+import net.x4a42.volksempfaenger.feedparser.FeedParserException;
+import net.x4a42.volksempfaenger.net.CacheInformation;
+import net.x4a42.volksempfaenger.net.FeedDownloader;
+import net.x4a42.volksempfaenger.net.NetException;
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+
+public class UpdateService extends IntentService {
+
+	private static long lastRun = 0;
+
+	public UpdateService() {
+		super(UpdateService.class.getSimpleName());
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		Uri podcast = intent.getData();
+		boolean extraFirstSync = intent.getBooleanExtra("first_sync", false);
+
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo netInfo = cm.getActiveNetworkInfo();
+		if (podcast == null
+				&& netInfo.getState() == NetworkInfo.State.DISCONNECTED) {
+			// If podcast == null, we sync all podcasts in the background. Thus
+			// we just return if background data is disabled.
+			return;
+		}
+
+		PodcastCursor cursor;
+		{
+			String[] projection = { Podcast._ID, Podcast.TITLE, Podcast.FEED,
+					Podcast.HTTP_LAST_MODIFIED, Podcast.HTTP_EXPIRES,
+					Podcast.HTTP_ETAG };
+
+			if (podcast != null) {
+				// Sync a single podcast
+				cursor = new PodcastCursor(getContentResolver().query(podcast,
+						projection, null, null, null));
+			} else {
+				lastRun = System.currentTimeMillis();
+				cursor = new PodcastCursor(getContentResolver().query(
+						VolksempfaengerContentProvider.PODCAST_URI, projection,
+						null, null, null));
+			}
+		}
+
+		if (cursor.getCount() == 0) {
+			// There are just no podcasts to update.
+			cursor.close();
+			return;
+		}
+
+		FeedDownloader feedDownloader = new FeedDownloader(this);
+		UpdateServiceHelper updateHelper = new UpdateServiceHelper(this);
+
+		long timeStart, timeEnd, timeFeedStart, timeFeedEnd;
+		timeStart = System.currentTimeMillis();
+
+		UpdateServiceStatus.startUpdate();
+
+		while (cursor.moveToNext()) {
+			UpdateServiceStatus.startUpdate(cursor.getUri());
+
+			Log.v(this, "Updating " + cursor.getTitle());
+
+			timeFeedStart = System.currentTimeMillis();
+
+			long podcastId = cursor.getId();
+
+			String podcastFeed = cursor.getFeed();
+
+			Feed feed = null;
+			CacheInformation cacheInfo = cursor.getCacheInformation();
+			if (podcast != null) {
+				// if a single podcast is updated, expiration should be ignored
+				cacheInfo.expires = 0;
+			}
+
+			try {
+				feed = feedDownloader.fetchFeed(podcastFeed, cacheInfo);
+			} catch (NetException e) {
+				// TODO Auto-generated catch block
+				Log.w(this, e);
+				UpdateServiceStatus.stopUpdate(cursor.getUri());
+				continue;
+			} catch (FeedParserException e) {
+				// TODO Auto-generated catch block
+				Log.w(this, e);
+				UpdateServiceStatus.stopUpdate(cursor.getUri());
+				continue;
+			}
+			PodcastHelper.updateCacheInformation(this, cursor.getUri(),
+					cacheInfo);
+			if (feed != null) {
+				updateHelper.updatePodcastFromFeed(podcastId, feed,
+						extraFirstSync);
+				timeFeedEnd = System.currentTimeMillis();
+				Log.v(this, "Updated " + feed.title + " (took "
+						+ (timeFeedEnd - timeFeedStart) + "ms)");
+			}
+
+			UpdateServiceStatus.stopUpdate(cursor.getUri());
+		}
+		cursor.close();
+
+		UpdateServiceStatus.stopUpdate();
+
+		timeEnd = System.currentTimeMillis();
+		Log.v(this, "Update took " + (timeEnd - timeStart) + "ms");
+
+		// start DownloadService to start automatic downloads if enabled
+		startService(new Intent(this, DownloadService.class));
+	}
+
+	public static long getLastRun() {
+		return lastRun;
+	}
+
+}
---
+++ src/net/x4a42/volksempfaenger/ui/ViewSubscriptionActivity.java
@@ -0,0 +1,215 @@
+package net.x4a42.volksempfaenger.ui;
+
+import net.x4a42.volksempfaenger.Log;
+import net.x4a42.volksempfaenger.R;
+import net.x4a42.volksempfaenger.data.Columns.Episode;
+import net.x4a42.volksempfaenger.data.Columns.Podcast;
+import net.x4a42.volksempfaenger.data.PodcastCursor;
+import net.x4a42.volksempfaenger.data.VolksempfaengerContentProvider;
+import net.x4a42.volksempfaenger.service.UpdateService;
+import net.x4a42.volksempfaenger.service.UpdateServiceStatus;
+import net.x4a42.volksempfaenger.service.UpdateServiceStatus.Status;
+import android.content.ContentUris;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ViewSubscriptionActivity extends EpisodeListActivity {
+
+	/* Static Variables */
+	private static final String PODCAST_WHERE = Podcast._ID + "=?";
+	private static final String EPISODE_WHERE = Episode.PODCAST_ID + "=?";
+
+	/* Subscription Attributes */
+	private long mId;
+	private Uri mUri;
+
+	/* View Attributes */
+	private PodcastLogoView mPodcastLogoView;
+	private TextView mPodcastDescriptionView;
+
+	/* Other Attributes */
+	private boolean mIsUpdating;
+	private UpdateServiceStatus.UiReceiver mUpdateReceiver;
+	private PodcastCursor podcastCursor;
+
+	/* Activity Lifecycle */
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		Intent intent = getIntent();
+		mUri = intent.getData();
+
+		if (mUri == null) {
+			mId = intent.getLongExtra("id", -1);
+			if (mId == -1) {
+				finish();
+				return;
+			}
+			mUri = ContentUris.withAppendedId(
+					VolksempfaengerContentProvider.PODCAST_URI, mId);
+		} else {
+			mId = ContentUris.parseId(mUri);
+		}
+
+		mUpdateReceiver = new UpdateReceiver();
+
+		if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+			ViewGroup header = (ViewGroup) getLayoutInflater().inflate(
+					R.layout.view_subscription_header, mEpisodeListView, false);
+			View separator = getLayoutInflater().inflate(
+					R.layout.horizontal_separator, mEpisodeListView, false);
+			mPodcastLogoView = (PodcastLogoView) header
+					.findViewById(R.id.podcast_logo);
+			mPodcastDescriptionView = (TextView) header
+					.findViewById(R.id.podcast_description);
+
+			mEpisodeListView.addHeaderView(header);
+			mEpisodeListView.addHeaderView(separator);
+		} else {
+			mPodcastLogoView = (PodcastLogoView) findViewById(R.id.podcast_logo);
+			mPodcastDescriptionView = (TextView) findViewById(R.id.podcast_description);
+		}
+		attachAdapter();
+
+		// Update podcast information
+		podcastCursor = new PodcastCursor(getContentResolver().query(
+				ContentUris.withAppendedId(
+						VolksempfaengerContentProvider.PODCAST_URI, mId),
+				new String[] {/* TODO */}, PODCAST_WHERE,
+				new String[] { String.valueOf(mId) }, null));
+
+		if (podcastCursor.getCount() == 0) {
+			// ID does not exist
+			finish();
+			return;
+		}
+		podcastCursor.moveToFirst();
+		setTitle(podcastCursor.getTitle());
+		mPodcastDescriptionView.setText(podcastCursor.getDescription());
+
+		mPodcastLogoView.setPodcastId(mId);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		UpdateServiceStatus.registerReceiver(mUpdateReceiver);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		UpdateServiceStatus.unregisterReceiver(mUpdateReceiver);
+	}
+
+	/* Content */
+	@Override
+	protected int getLayout() {
+		return R.layout.view_subscription;
+	}
+
+	@Override
+	protected CursorLoader getCursorLoader() {
+		return new CursorLoader(ViewSubscriptionActivity.this,
+				VolksempfaengerContentProvider.EPISODE_URI, EPISODE_PROJECTION,
+				EPISODE_WHERE, new String[] { String.valueOf(mId) },
+				EPISODE_SORT);
+	}
+
+	private class UpdateReceiver extends UpdateServiceStatus.UiReceiver {
+
+		public UpdateReceiver() {
+			setActivity(ViewSubscriptionActivity.this);
+		}
+
+		@Override
+		public void receiveUi(Status status) {
+			Log.v(this, status.toString());
+			if (status.isUpdating()) {
+				if (!mIsUpdating && mUri.equals(status.getUri())) {
+					mIsUpdating = true;
+					invalidateOptionsMenu();
+				}
+			} else {
+				if (mIsUpdating && mUri.equals(status.getUri())) {
+					mIsUpdating = false;
+					invalidateOptionsMenu();
+				}
+			}
+		}
+	}
+
+	/* Menu */
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.view_subscription, menu);
+		ActivityHelper.addGlobalMenu(this, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		boolean result = super.onPrepareOptionsMenu(menu);
+
+		MenuItem update = menu.findItem(R.id.item_update);
+		if (mIsUpdating) {
+			update.setActionView(R.layout.actionbar_updating);
+		} else {
+			update.setActionView(null);
+		}
+
+		return result;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		boolean result = super.onOptionsItemSelected(item);
+		if (result) {
+			return result;
+		}
+		Intent intent;
+		switch (item.getItemId()) {
+
+		case R.id.item_update:
+			intent = new Intent(this, UpdateService.class);
+			intent.setData(mUri);
+			startService(intent);
+			return true;
+
+		case R.id.item_delete:
+			intent = new Intent(this, DeleteSubscriptionActivity.class);
+			intent.putExtra("id", mId);
+			startActivity(intent);
+			return true;
+
+		case R.id.item_website:
+			intent = new Intent(Intent.ACTION_VIEW,
+					podcastCursor.getWebsiteUri());
+			startActivity(intent);
+			return true;
+
+		case R.id.item_share:
+			intent = new Intent(Intent.ACTION_SEND);
+			intent.setType("text/plain");
+			intent.putExtra(Intent.EXTRA_TEXT, podcastCursor.getWebsite());
+			startActivity(Intent.createChooser(intent,
+					getString(R.string.title_share)));
+			return true;
+		default:
+			return false;
+		}
+	}
+}
