c0872e5968a30dbcecca1c6653ddbb1ed56f773b
Vincent Sch??ler <vincent@vschuessler.org>
2012-07-30 14:14:16 +0200

    Store flattr url in UpdateService

--- src/net/x4a42/volksempfaenger/data/Constants.java
+++ src/net/x4a42/volksempfaenger/data/Constants.java
@@ -7,8 +7,10 @@ public class Constants {
 	public static final int EPISODE_STATE_LISTENING = 3;
 	public static final int EPISODE_STATE_LISTENED = 4;
 
+	// not on flattr
+	public static final int FLATTR_STATE_NONE = 0;
 	// unflattred
-	public static final int FLATTR_STATE_NEW = 0;
+	public static final int FLATTR_STATE_NEW = 1;
 	// user has flattred the episode, but the actual flattr action did not happen yet
 	public static final int FLATTR_STATE_USER_FLATTRED = 10;
 	// the episode got flattred successfuly
