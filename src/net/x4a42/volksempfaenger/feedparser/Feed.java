b9d46d445dc08e1a375e934036874b725cbb9d19
Vincent Sch??ler <vincent@vschuessler.org>
2012-08-08 10:20:32 +0200

    Make the FeedParser event-based internally (see #78)

--- src/net/x4a42/volksempfaenger/feedparser/Feed.java
+++ src/net/x4a42/volksempfaenger/feedparser/Feed.java
@@ -3,7 +3,24 @@ package net.x4a42.volksempfaenger.feedparser;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Feed {
+public class Feed implements Cloneable {
 	public String url, title, website, description, image;
 	public List<FeedItem> items = new ArrayList<FeedItem>();
+
+	public void reset() {
+		url = null;
+		title = null;
+		website = null;
+		description = null;
+		image = null;
+		items = null;
+	}
+
+	public Object clone() {
+		try {
+			return super.clone();
+		} catch (CloneNotSupportedException e) {
+			return null;
+		}
+	}
 }
