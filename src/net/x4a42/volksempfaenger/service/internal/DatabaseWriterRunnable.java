aa79378c796ceba5ddca5a499fa84ab5c81512fa
Julian Brost <julian@0x4a42.net>
2012-08-09 22:54:55 +0200

    Improved new UpdateService implementation but still no database updates.

--- src/net/x4a42/volksempfaenger/service/internal/DatabaseWriterRunnable.java
+++ src/net/x4a42/volksempfaenger/service/internal/DatabaseWriterRunnable.java
@@ -1,6 +1,13 @@
 package net.x4a42.volksempfaenger.service.internal;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import net.x4a42.volksempfaenger.Log;
+
 public class DatabaseWriterRunnable extends UpdateRunnable {
+
+	private static final String TAG = "UpdateService";
 
 	public DatabaseWriterRunnable(UpdateState update) {
 		super(update);
@@ -9,18 +16,58 @@ public class DatabaseWriterRunnable extends UpdateRunnable {
 	@Override
 	public void run() {
 
-		// updateService.getUpdateHelper().updatePodcastFromFeed(podcast.id,
-		// feed,
-		// podcast.firstSync);
-
-		// TODO
+		BlockingQueue<Object> queue = getUpdate().getDatabaseWriterQueue();
+		long startTime = -1;
 
 		try {
-			Thread.sleep(10000);
+			while (true) {
+				Object o = queue.poll(4, TimeUnit.SECONDS);
+				if (o == null) {
+					if (getUpdate().getRemainingFeedCounter() == 0) {
+						break;
+					} else {
+						continue;
+					}
+				} else {
+					if (startTime == -1) {
+						startTime = System.currentTimeMillis();
+						Log.i(TAG, "Started writing to database");
+					}
+					ensureTransaction();
+					handleObject(o);
+				}
+			}
 		} catch (InterruptedException e) {
+			Log.i(this, "Exception", e);
 		}
-		getUpdate().getUpdateService().stopSelf(getUpdate().getStartId());
+
+		finishTransaction();
+
+		if (startTime != -1) {
+			long endTime = System.currentTimeMillis();
+			Log.i(TAG, "Finished writing to database (took "
+					+ (endTime - startTime) + "ms)");
+		}
+
+		getUpdate().stopUpdate();
 
 	}
 
+	private void ensureTransaction() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void finishTransaction() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void handleObject(Object o) {
+		// TODO Auto-generated method stub
+		// Log.d(this, o.toString());
+		// updateService.getUpdateHelper().updatePodcastFromFeed(podcast.id,
+		// feed,
+		// podcast.firstSync);
+	}
 }
--- src/net/x4a42/volksempfaenger/service/internal/UpdateState.java
+++ src/net/x4a42/volksempfaenger/service/internal/UpdateState.java
@@ -1,18 +1,32 @@
 package net.x4a42.volksempfaenger.service.internal;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import net.x4a42.volksempfaenger.service.UpdateService;
 import android.content.Intent;
+import android.util.Log;
 
 public class UpdateState {
+
+	private static final String TAG = "UpdateService";
 
 	private UpdateService updateService;
 	private Intent intent;
 	private int startId;
+	private BlockingQueue<Object> databaseWriterQueue = new LinkedBlockingQueue<Object>();
+	private AtomicInteger remainingFeedCounter = new AtomicInteger();
+	private AtomicBoolean remainingFeedCounterSet = new AtomicBoolean();
+	private long startTime;
 
 	public UpdateState(UpdateService updateService, Intent intent, int startId) {
 		this.updateService = updateService;
 		this.intent = intent;
 		this.startId = startId;
+		startTime = System.currentTimeMillis();
+		Log.i(TAG, "Starting update");
 	}
 
 	public UpdateService getUpdateService() {
@@ -25,6 +39,30 @@ public class UpdateState {
 
 	public int getStartId() {
 		return startId;
+	}
+
+	public BlockingQueue<Object> getDatabaseWriterQueue() {
+		return databaseWriterQueue;
+	}
+
+	public void setRemainingFeedCounter(int value) {
+		if (remainingFeedCounterSet.compareAndSet(false, true)) {
+			remainingFeedCounter.addAndGet(value);
+		}
+	}
+
+	public void decrementRemainingFeedCounter() {
+		remainingFeedCounter.decrementAndGet();
+	}
+
+	public int getRemainingFeedCounter() {
+		return remainingFeedCounter.get();
+	}
+
+	public void stopUpdate() {
+		long endTime = System.currentTimeMillis();
+		Log.i(TAG, "Finished update (took " + (endTime - startTime) + "ms)");
+		getUpdateService().stopSelf(startId);
 	}
 
 }
