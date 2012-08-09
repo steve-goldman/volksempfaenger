0f60a9d7a283005750feddf2c94461b4d997096f
Vincent Sch??ler <vincent@vschuessler.org>
2012-08-09 17:28:30 +0200

    Create new objects for FeedItem and Enclosure in FeedParser

--- src/net/x4a42/volksempfaenger/feedparser/Enclosure.java
+++ src/net/x4a42/volksempfaenger/feedparser/Enclosure.java
@@ -1,27 +1,8 @@
 package net.x4a42.volksempfaenger.feedparser;
 
-public class Enclosure implements Cloneable {
+public class Enclosure {
 	public String title, url, mime, file;
 	public long size;
 	public FeedItem feedItem;
 	public int state;
-
-	public void reset() {
-		title = null;
-		url = null;
-		mime = null;
-		file = null;
-		size = 0;
-		feedItem = null;
-		state = 0;
-	}
-
-	public Object clone() {
-		try {
-			return super.clone();
-		} catch (CloneNotSupportedException e) {
-			return null;
-		}
-	}
-
 }
