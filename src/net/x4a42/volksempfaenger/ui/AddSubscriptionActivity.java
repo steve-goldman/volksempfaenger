d3119b22c1da342bf39c9c9e424dbeff2775e5ab
Vincent Sch??ler <vincent@vschuessler.org>
2012-08-17 12:01:05 +0200

    Remove DiscoverFragment and replace it with AddSubscriptionActivity

--- src/net/x4a42/volksempfaenger/ui/AddSubscriptionActivity.java
+++ src/net/x4a42/volksempfaenger/ui/AddSubscriptionActivity.java
@@ -1,29 +1,55 @@
 package net.x4a42.volksempfaenger.ui;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 
-import net.x4a42.volksempfaenger.Log;
 import net.x4a42.volksempfaenger.R;
+import net.x4a42.volksempfaenger.Utils;
+import net.x4a42.volksempfaenger.VolksempfaengerApplication;
+import net.x4a42.volksempfaenger.feedparser.GpodderJsonReader;
+import net.x4a42.volksempfaenger.feedparser.GpodderJsonReaderListener;
+import net.x4a42.volksempfaenger.misc.StorageException;
+import net.x4a42.volksempfaenger.net.FileDownloader;
+import net.x4a42.volksempfaenger.net.NetException;
+import android.app.ActionBar;
 import android.app.Activity;
-import android.content.ClipData;
-import android.content.ClipboardManager;
-import android.net.Uri;
+import android.app.SearchManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.WindowManager.LayoutParams;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
 
 public class AddSubscriptionActivity extends Activity implements
-		OnClickListener {
+		OnUpPressedCallback, OnItemClickListener {
 
-	private EditText editTextUrl;
-	private Button buttonAdd;
-	private Button buttonCancel;
+	private ImageLoader imageLoader;
+	private final static DisplayImageOptions options = new DisplayImageOptions.Builder()
+			.showStubImage(R.drawable.default_logo)
+			.showImageForEmptyUri(R.drawable.default_logo).cacheInMemory()
+			.cacheOnDisc().imageScaleType(ImageScaleType.POWER_OF_2).build();
+	private File toplistFile;
+	private AsyncTask<Void, HashMap<String, String>, Void> loadTask;
+	private ListView popularList;
+	private MenuItem searchMenuItem;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -31,51 +57,37 @@ public class AddSubscriptionActivity extends Activity implements
 		setTitle(R.string.title_add_subscription);
 		setContentView(R.layout.add_subscription);
 
-		LayoutParams params = getWindow().getAttributes();
-		params.width = LayoutParams.MATCH_PARENT;
-		getWindow().setAttributes(params);
+		ActionBar actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
 
-		editTextUrl = (EditText) findViewById(R.id.edittext_url);
-		buttonAdd = (Button) findViewById(R.id.button_add);
-		buttonCancel = (Button) findViewById(R.id.button_cancel);
+		popularList = (ListView) findViewById(R.id.popular_list);
+		popularList.setOnItemClickListener(this);
 
-		buttonAdd.setOnClickListener(this);
-		buttonCancel.setOnClickListener(this);
+		toplistFile = new File(getFilesDir(), "popular.json");
+
+		imageLoader = ((VolksempfaengerApplication) getApplication()).imageLoader;
+		if (toplistFile.exists()) {
+			loadTask = new LoadPopularListTask()
+					.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+		}
+		new RefreshPopularListTask()
+				.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
 	}
 
 	@Override
-	public void onStart() {
-		super.onStart();
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.discover, menu);
+		searchMenuItem = menu.findItem(R.id.menu_search);
 
-		Uri data = getIntent().getData();
-		if (data != null) {
-			editTextUrl.setText(data.toString());
-		} else {
-			// see if there is a link in the clipboard
-			ClipboardManager cm = (ClipboardManager) getSystemService(Activity.CLIPBOARD_SERVICE);
-			ClipData clip = cm.getPrimaryClip();
-			if (clip != null) {
-				ClipData.Item item = clip.getItemAt(0);
-				if (item != null && item.getText() != null) {
-					String text = item.getText().toString();
-					try {
-						URL url = new URL(text);
-						editTextUrl.setText(url.toString());
-					} catch (MalformedURLException e) {
-						Log.e(this, "no url");
-						Matcher matcher = Pattern
-								.compile(
-										"\\b(http|https):[/]*[\\w-]+\\.[\\w./?&@#-]+")
-								.matcher(text);
-						if (matcher.find()) {
-							Log.e(this, "url found");
-							editTextUrl.setText(matcher.group());
-						}
-						Log.e(this, "finish");
-					}
-				}
-			}
-		}
+		SearchView searchView = (SearchView) menu.findItem(R.id.menu_search)
+				.getActionView();
+		SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+		searchView.setSearchableInfo(searchManager
+				.getSearchableInfo(new ComponentName(getPackageName(),
+						SearchActivity.class.getName())));
+		ActivityHelper.addGlobalMenu(this, menu);
+		return true;
 	}
 
 	@Override
@@ -84,21 +96,147 @@ public class AddSubscriptionActivity extends Activity implements
 		ExternalStorageHelper.assertExternalStorageWritable(this);
 	}
 
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.button_add:
-			subscribeToFeed();
-			return;
-		case R.id.button_cancel:
-			finish();
-			return;
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		default:
+			return ActivityHelper.handleGlobalMenu(this, item);
 		}
 	}
 
-	private void subscribeToFeed() {
-		String feedUrl = editTextUrl.getText().toString();
-
-		new AddFeedTask(getApplicationContext()).execute(feedUrl);
-		finish();
+	@Override
+	public void onUpPressed() {
+		Intent intent = NavUtils.getParentActivityIntent(this);
+		intent.putExtra("tag", MainActivity.subscriptionsTag);
+		NavUtils.navigateUpTo(this, intent);
 	}
+
+	private class LoadPopularListTask extends LoadGpodderListTask {
+
+		public LoadPopularListTask() {
+			super(AddSubscriptionActivity.this, imageLoader, options,
+					new SetAdapterCallback() {
+						@Override
+						public void setAdapter(ListAdapter adapter) {
+							popularList.setAdapter(adapter);
+						}
+					});
+		}
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			try {
+				synchronized (toplistFile) {
+					BufferedReader fileReader = new BufferedReader(
+							new FileReader(toplistFile));
+					new GpodderJsonReader(fileReader,
+							new GpodderJsonReaderListener() {
+
+								@SuppressWarnings("unchecked")
+								@Override
+								public void onPodcast(
+										HashMap<String, String> podcast) {
+									publishProgress(podcast);
+								}
+							}).read();
+				}
+			} catch (IOException e) {
+				// TODO handle error
+				return null;
+			}
+			return null;
+		}
+	}
+
+	private class RefreshPopularListTask extends AsyncTask<Void, Void, Boolean> {
+
+		@Override
+		protected Boolean doInBackground(Void... params) {
+			try {
+				synchronized (toplistFile) {
+					// 1 day
+					final long minTime = System.currentTimeMillis() - 24 * 60
+							* 60 * 1000;
+					if (toplistFile.exists()
+							&& toplistFile.lastModified() < minTime) {
+						return false;
+					}
+				}
+				File tempFile = File.createTempFile("popular", ".json",
+						getCacheDir());
+				FileDownloader downloader = new FileDownloader(
+						AddSubscriptionActivity.this);
+				downloader
+						.fetchFile(
+								"http://gpodder.net/toplist/100.json?scale_logo="
+										+ Utils.dpToPx(
+												AddSubscriptionActivity.this,
+												64), tempFile);
+				synchronized (toplistFile) {
+					tempFile.renameTo(toplistFile);
+				}
+			} catch (NetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return false;
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return false;
+			} catch (StorageException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return false;
+			}
+			return true;
+		}
+
+		@Override
+		protected void onPostExecute(Boolean result) {
+			if (!result) {
+				return;
+			}
+			if (loadTask != null
+					&& loadTask.getStatus() != AsyncTask.Status.FINISHED) {
+				loadTask.cancel(true);
+			}
+
+			loadTask = new LoadPopularListTask()
+					.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+		}
+
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> list, View view, int position,
+			long id) {
+
+		@SuppressWarnings("unchecked")
+		HashMap<String, String> row = (HashMap<String, String>) list
+				.getItemAtPosition(position);
+		Intent intent = new Intent(this, DiscoverDetailActivity.class);
+		intent.putExtra(GpodderJsonReader.KEY_TITLE,
+				row.get(GpodderJsonReader.KEY_TITLE));
+		intent.putExtra(GpodderJsonReader.KEY_DESCRIPTION,
+				row.get(GpodderJsonReader.KEY_DESCRIPTION));
+		intent.putExtra(GpodderJsonReader.KEY_SCALED_LOGO,
+				row.get(GpodderJsonReader.KEY_SCALED_LOGO));
+		intent.putExtra(GpodderJsonReader.KEY_URL,
+				row.get(GpodderJsonReader.KEY_URL));
+		intent.putExtra(GpodderJsonReader.KEY_WEBSITE,
+				row.get(GpodderJsonReader.KEY_WEBSITE));
+		startActivity(intent);
+	}
+
+	@Override
+	public boolean onKeyUp(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_SEARCH) {
+			if (searchMenuItem != null) {
+				searchMenuItem.expandActionView();
+				return true;
+			}
+		}
+		return false;
+	}
+
 }
