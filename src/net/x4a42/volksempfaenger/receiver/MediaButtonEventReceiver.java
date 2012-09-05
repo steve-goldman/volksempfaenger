a2d46324628b712639df9a2529b80732f2edb5db
Julian Brost <julian@0x4a42.net>
2012-09-05 17:04:06 +0200

    Please give us a big round of facepalm for fixing the problem with media button intents being received twice. One was the DOWN and the other one the UP intent, fixes #5.

--- src/net/x4a42/volksempfaenger/receiver/MediaButtonEventReceiver.java
+++ src/net/x4a42/volksempfaenger/receiver/MediaButtonEventReceiver.java
@@ -7,10 +7,10 @@ import android.content.Intent;
 import android.view.KeyEvent;
 
 public class MediaButtonEventReceiver extends BroadcastReceiver {
-	static boolean ignoreNext = false;
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
+
 		if (!Intent.ACTION_MEDIA_BUTTON.equals(intent.getAction())) {
 			return;
 		}
@@ -21,28 +21,24 @@ public class MediaButtonEventReceiver extends BroadcastReceiver {
 		if (event == null) {
 			return;
 		}
-		// FIXME
-		// intents are being received twice
-		// we do not know why, so ignore every second intent
-		if (ignoreNext) {
-			ignoreNext = false;
-			return;
-		} else {
-			ignoreNext = true;
+
+		if (event.getAction() == KeyEvent.ACTION_UP) {
+			switch (event.getKeyCode()) {
+			case KeyEvent.KEYCODE_MEDIA_PLAY:
+				break;
+			case KeyEvent.KEYCODE_MEDIA_PAUSE:
+				break;
+			case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
+				Intent keyIntent = new Intent(context, PlaybackService.class);
+				keyIntent.setAction(Intent.ACTION_MEDIA_BUTTON);
+				keyIntent.putExtra(Intent.EXTRA_KEY_EVENT, event);
+				context.startService(keyIntent);
+				break;
+			default:
+				return;
+			}
 		}
-		switch (event.getKeyCode()) {
-		case KeyEvent.KEYCODE_MEDIA_PLAY:
-			break;
-		case KeyEvent.KEYCODE_MEDIA_PAUSE:
-			break;
-		case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
-			Intent keyIntent = new Intent(context, PlaybackService.class);
-			keyIntent.setAction(Intent.ACTION_MEDIA_BUTTON);
-			keyIntent.putExtra(Intent.EXTRA_KEY_EVENT, event);
-			context.startService(keyIntent);
-			break;
-		default:
-			return;
-		}
+
 	}
+
 }
