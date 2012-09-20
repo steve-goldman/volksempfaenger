8fac94097c766d7bbb5db6434d121594ecfcd3a3
Julian Brost <julian@0x4a42.net>
2012-09-20 11:19:12 +0200

    Show version on about screen and move URLs to resources, see #35.

--- src/net/x4a42/volksempfaenger/ui/SettingsActivity.java
+++ src/net/x4a42/volksempfaenger/ui/SettingsActivity.java
@@ -2,11 +2,15 @@ package net.x4a42.volksempfaenger.ui;
 
 import java.util.List;
 
+import net.x4a42.volksempfaenger.PreferenceKeys;
 import net.x4a42.volksempfaenger.R;
+import net.x4a42.volksempfaenger.VolksempfaengerApplication;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
 
 public class SettingsActivity extends PreferenceActivity {
 
@@ -41,6 +45,15 @@ public class SettingsActivity extends PreferenceActivity {
 		public void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
 			addPreferencesFromResource(R.xml.preference_about);
+
+			PreferenceScreen prefScreen = getPreferenceScreen();
+			VolksempfaengerApplication application = (VolksempfaengerApplication) getActivity()
+					.getApplication();
+
+			Preference version = prefScreen
+					.findPreference(PreferenceKeys.ABOUT_VERSION);
+
+			version.setSummary(application.getVersionName());
 		}
 
 	}
