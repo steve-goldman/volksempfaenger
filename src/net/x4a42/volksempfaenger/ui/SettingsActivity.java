a3ef862ad4099ae9377034261424b338a2805001
Vincent Sch??ler <vincent@vschuessler.org>
2012-09-20 11:20:28 +0200

    Start implementing Flattr authorization (see #30)

--- src/net/x4a42/volksempfaenger/ui/SettingsActivity.java
+++ src/net/x4a42/volksempfaenger/ui/SettingsActivity.java
@@ -3,7 +3,9 @@ package net.x4a42.volksempfaenger.ui;
 import java.util.List;
 
 import net.x4a42.volksempfaenger.R;
-
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
@@ -43,6 +45,23 @@ public class SettingsActivity extends PreferenceActivity {
 			addPreferencesFromResource(R.xml.preference_about);
 		}
 
+	}
+
+	public static class FlattrCallbackProxyActivity extends Activity {
+		@Override
+		public void onStart() {
+			super.onStart();
+			Uri uri = getIntent().getData();
+			if (uri != null) {
+				Intent intent = new Intent(this, SettingsActivity.class);
+				intent.putExtra(EXTRA_SHOW_FRAGMENT,
+						FlattrSettingsFragment.class.getName());
+				Bundle bundle = new Bundle();
+				bundle.putString("code", uri.getQueryParameter("code"));
+				intent.putExtra(EXTRA_SHOW_FRAGMENT_ARGUMENTS, bundle);
+				startActivity(intent);
+			}
+		}
 	}
 
 }
