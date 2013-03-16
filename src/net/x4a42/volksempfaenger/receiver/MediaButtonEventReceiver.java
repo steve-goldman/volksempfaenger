7d1b82be5a8f548714539077f553346e9d255a9d
Julian Brost <julian@0x4a42.net>
2013-03-16 23:23:12 +0100

    Removed old PlaybackService.

--- src/net/x4a42/volksempfaenger/receiver/MediaButtonEventReceiver.java
+++ src/net/x4a42/volksempfaenger/receiver/MediaButtonEventReceiver.java
@@ -1,6 +1,5 @@
 package net.x4a42.volksempfaenger.receiver;
 
-import net.x4a42.volksempfaenger.service.PlaybackService;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -22,22 +21,22 @@ public class MediaButtonEventReceiver extends BroadcastReceiver {
 			return;
 		}
 
-		if (event.getAction() == KeyEvent.ACTION_UP) {
-			switch (event.getKeyCode()) {
-			case KeyEvent.KEYCODE_MEDIA_PLAY:
-				break;
-			case KeyEvent.KEYCODE_MEDIA_PAUSE:
-				break;
-			case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
-				Intent keyIntent = new Intent(context, PlaybackService.class);
-				keyIntent.setAction(Intent.ACTION_MEDIA_BUTTON);
-				keyIntent.putExtra(Intent.EXTRA_KEY_EVENT, event);
-				context.startService(keyIntent);
-				break;
-			default:
-				return;
-			}
-		}
+		// if (event.getAction() == KeyEvent.ACTION_UP) {
+		// switch (event.getKeyCode()) {
+		// case KeyEvent.KEYCODE_MEDIA_PLAY:
+		// break;
+		// case KeyEvent.KEYCODE_MEDIA_PAUSE:
+		// break;
+		// case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
+		// Intent keyIntent = new Intent(context, PlaybackService.class);
+		// keyIntent.setAction(Intent.ACTION_MEDIA_BUTTON);
+		// keyIntent.putExtra(Intent.EXTRA_KEY_EVENT, event);
+		// context.startService(keyIntent);
+		// break;
+		// default:
+		// return;
+		// }
+		// }
 
 	}
 
