4886144dd652f10ead96d4e7f3bd6c81ce586d96
Kolja Dummann <k.dummann@gmail.com>
2013-05-20 16:52:29 +0200

    delete download when deleting a podcast
    fix #182

--- src/net/x4a42/volksempfaenger/ui/DeleteSubscriptionActivity.java
+++ src/net/x4a42/volksempfaenger/ui/DeleteSubscriptionActivity.java
@@ -1,8 +1,14 @@
 package net.x4a42.volksempfaenger.ui;
 
+import android.app.DownloadManager;
+import android.content.ContentResolver;
+import android.content.Context;
 import net.x4a42.volksempfaenger.R;
 import net.x4a42.volksempfaenger.Utils;
+import net.x4a42.volksempfaenger.data.Columns;
 import net.x4a42.volksempfaenger.data.Columns.Podcast;
+import net.x4a42.volksempfaenger.data.EpisodeCursor;
+import net.x4a42.volksempfaenger.data.EpisodeHelper;
 import net.x4a42.volksempfaenger.data.VolksempfaengerContentProvider;
 import android.app.Activity;
 import android.content.ContentUris;
@@ -89,7 +95,32 @@ public class DeleteSubscriptionActivity extends Activity implements
 	}
 
 	public void deletePodcast() {
+
+		ContentResolver resolver = this.getContentResolver();
+		Cursor cursor = resolver.query(
+				VolksempfaengerContentProvider.EPISODE_URI,
+				new String[] {Columns.Episode._ID, Columns.Episode.DOWNLOAD_BYTES_DOWNLOADED_SO_FAR }, Columns.Episode.PODCAST_ID + "=" + String.valueOf(id), null, null);
+
+		EpisodeCursor episodeCursor = new EpisodeCursor(cursor);
+
+		if(episodeCursor.moveToFirst())
+		{
+			do{
+				if(episodeCursor.getDownloadDone() > 0)
+				{
+					EpisodeHelper
+							.deleteDownload(getContentResolver(),
+									(DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE),
+									episodeCursor.getId()
+									);
+				}
+			} while (episodeCursor.moveToNext());
+		}
+
+		episodeCursor.close();
+
 		int result = getContentResolver().delete(uri, null, null);
+
 		if (result > 0) {
 			// row was deleted
 			Toast.makeText(this, R.string.message_podcast_successfully_deleted,
